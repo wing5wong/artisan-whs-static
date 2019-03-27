470e1fdf44230bc011a4a5da04f3205d4ff78de4
Sean Anderson <wing5wong@hotmail.com>
2019-03-27 15:42:47 +1300

    Update index.blade.php
    show grant

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Artisan Static for Jigsaw</h1>
+    <h1>Welcome to Artisan Static for Jigsaw, Grant</h1>
 
     <h2>Asset Compilation Test</h2>
 
