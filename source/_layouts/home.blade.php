516ff8edf82740593cf97366cda5a64a9dbb6c01
WHS-Techs <ohmy@whs.ac.nz>
2019-03-28 16:23:14 +1300

    announcement

--- source/_layouts/home.blade.php
+++ source/_layouts/home.blade.php
@@ -108,19 +108,17 @@
        background: #b00
      }
    </style>
- <pop:content from="green-box">
 
    <div class="decorated wrap wrap--notification <pop:emergency>emergency</pop:emergency>" style="padding: 1em 0; position: relative; z-index: 99; box-shadow: 0 8px 6px -6px #111">
   <div class="row column" style="display: flex; align-items:center; justify-content: center; text-align: center;flex-wrap: wrap">
 
-    <pop:announcement wrap="p" editable="true" />
+    An announcement that is happening
     	<a class="btn" href="<pop:permalink />">Read the full announcement</a>
     
 
     
 	</div>
 </div>
-</pop:content>
 
 
 
