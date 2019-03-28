cc6fac3e61c7bbd923b77dbcc2954b9628971e7d
WHS-Techs <ohmy@whs.ac.nz>
2019-03-28 16:03:20 +1300

    stuff

--- source/_layouts/home.blade.php
+++ source/_layouts/home.blade.php
@@ -184,11 +184,304 @@
              
 <div class="row no-gutters">   <pop:content>     <div class="col p-5 d-none d-md-block bg-attached" <pop:feature_image> style="min-height:45vh; background-image: url(<pop:value.src />)"</pop:feature_image>>   </div>   </pop:content>   </div>
 
-  <pop:include template="includes/_partials/pages/home/news" />
+
+
+
+
+
+<div class="row no-gutters">
+  <div class="col">
+    <div class="p-5 bg-white text-center text-green d-flex flex-column justify-content-around align-items-center">
+      <h1 class="mb-5">News &amp; Events</h1>
+      <pop:content from="news">
+        <pop:intro_text wrap="p" />
+      </pop:content>
+      <a class="mt-5 btn btn-outline-dark" href="/news">Read more.</a>
+    </div>
+    </div>
+    
+          <!-- Slider main container -->
+<div class="d-none col d-md-flex swiper-container bg-white text-center text-green flex-column justify-content-around align-items-center triangle triangle-right triangle-white">
+  <!-- Additional required wrapper -->
+<div class="swiper-wrapper">
+      <!-- Slides -->
+  <pop:content from="news">
+    <pop:entries limit="3">
+      <div class="swiper-slide p-5 d-flex align-items-center justify-content-center" style="background-image: url(<pop:image.src resize="limit" width="936" height="500" />); background-size: cover; width: 100%; height: 500px;">
+        <a href="<pop:permalink />" class="btn btn-light"><pop:title/></a>
+      </div>
+  </pop:entries>
+    </pop:content>
+  </div>
+<div class="swiper-button-next"></div>
+<div class="swiper-button-prev"></div>
+  </div>
+</div>
         
 
-     <pop:include template="/includes/_partials/footers/main" />   
+
+
+
+
+
+<footer class="py-5" style="background-image: linear-gradient(to top, transparent, #fff 85%),url(/images/river.jpg);min-height: 700px;background-size: 100%, cover;">
+  <!--<div class="row">
+    <div class="col-12 text-center">
+      <img src="<pop:asset name='images/life/whs-banner.png' />" class="mb-5 px-3" style="max-width: 100%">
+    </div>
+</div>-->
+<div class="container decorated mb-5"></div>
+<div class="container">
+  <div class="row">
+    <div class="col-12 text-center">
+      <img src="<pop:asset name='images/life/whs-banner.png' />" class="mb-5" style="max-width: 100%;"/>
+      <!--<img src="<pop:asset name='logo_vertical_t.png' />" width="160" class="mb-5">-->
+    </div>
+    <div class="col-6 col-md">
+      <h5>School</h5>
+      <ul class="list-unstyled text-small">
+        <li>
+          <a href="https://kamar.whs.ac.nz/notices" target="_blank">
+            Daily Notices
+          </a>
+        </li>
       
-    <pop:include template="includes/_partials/scripts/main" />
+        <li>
+          <a href="http://nzcurriculum.tki.org.nz/" target="_blank">
+            New Zealand Curriculum
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.minedu.govt.nz/" target="_blank">
+            NZ Ministry of Education
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.nzqa.govt.nz/" target="_blank">
+            New Zealand Qualifications Authority (NZQA)
+          </a>
+        </li>
+      
+        <li>
+          <a href="https://www.facebook.com/pages/Wanganui-High-School-Sports/168112976671150" target="_blank">
+            WHS Sports Facebook Page
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Students</h5>
+      <ul class="list-unstyled text-small">
+        <li>
+          <a href="http://students.wanganuihigh.school.nz/student/index.php" target="_blank">
+            Kamar Portal
+          </a>
+        </li>
+      
+        <li>
+          <a href="https://outlook.office365.com/owa/" target="_blank">
+            Webmail
+          </a>
+        </li>
+      
+        <li>
+          <a href="https://portal.office.com/Home" target="_blank">
+            Office 365
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://nzcurriculum.tki.org.nz/" target="_blank">
+            New Zealand Curriculum
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.minedu.govt.nz/" target="_blank">
+            NZ Ministry of Education
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.nzqa.govt.nz/" target="_blank">
+            New Zealand Qualifications Authority (NZQA)
+          </a>
+        </li>
+      
+        <li>
+          <a href="https://www.facebook.com/pages/Wanganui-High-School-Sports/168112976671150" target="_blank">
+            WHS Sports Facebook Page
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.studyit.org.nz/" target="_blank">
+            Study It - Study Resources for NCEA Students
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.nzhistory.net.nz/" target="_blank">
+            NZ History Online
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.teara.govt.nz/" target="_blank">
+            Te Ara - New Zealand Encyclopedia
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Parents</h5>
+      <ul class="list-unstyled text-small">
+        <li>
+          <a href="http://students.wanganuihigh.school.nz/student/index.php" target="_blank">
+            Kamar Portal
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://nzcurriculum.tki.org.nz/" target="_blank">
+            New Zealand Curriculum
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.minedu.govt.nz/" target="_blank">
+            NZ Ministry of Education
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.nzqa.govt.nz/" target="_blank">
+            New Zealand Qualifications Authority (NZQA)
+          </a>
+        </li>
+      
+        <li>
+          <a href="https://www.facebook.com/pages/Wanganui-High-School-Sports/168112976671150" target="_blank">
+            WHS Sports Facebook Page
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>International</h5>
+      <ul class="list-unstyled text-small">
+        <li>
+          <a href="http://www.education-newzealand.org/" target="_blank">
+            Education in NZ - An Insiders Guide to Studying Abroad
+          </a>
+        </li>
+      
+        <li>
+          <a href="https://outlook.office365.com/owa/" target="_blank">
+            Webmail
+          </a>
+        </li>
+      
+        <li>
+          <a href="https://portal.office.com/Home" target="_blank">
+            Office 365
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.tourism.org.nz/" target="_blank">
+            New Zealand Tourism Information
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.newzealand.com/int/" target="_blank">
+            New Zealand Destination Website
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.studyit.org.nz/" target="_blank">
+            Study It - Study Resources for NCEA Students
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.teara.govt.nz/" target="_blank">
+            Te Ara - New Zealand Encyclopedia
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-md">
+      <h5>Whanganui/NZ</h5>
+      <ul class="list-unstyled text-small">
+        <li>
+          <a href="http://whanganuinz.com/" target="_blank">
+            Official Wanganui Website
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.wanganuionline.com/" target="_blank">
+            Wanganui Online
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.tourism.org.nz/" target="_blank">
+            New Zealand Tourism Information
+          </a>
+        </li>
+      
+        <li>
+          <a href="http://www.newzealand.com/int/" target="_blank">
+            New Zealand Destination Website
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  </div>
+</footer>
+
+      
+
+
+
+
+
+
+
+
+
+<script>
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: true,
+    effect: 'fade',
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  })
+      
+  </script>
+  
+  <script crossorigin="anonymous" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script> 
+
+  <script src="/assets/js/bootstrap.min.js"></script>
+
+
+
+
+
+
+
+
   </body>
 </html>
