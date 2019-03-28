782ab1c0061737305ccc98401d3a7c8c01ad2962
WHS-Techs <ohmy@whs.ac.nz>
2019-03-28 14:12:06 +1300

    start adding homepage

---
+++ source/_layouts/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+  <meta content="" name="description">
+  <meta content="" name="author">
+  <link href="../../../../favicon.ico" rel="icon">
+  <title>Whanganui High School</title><!-- Bootstrap core CSS -->
+  
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/css/swiper.min.css" rel="stylesheet" />
+  <pop:bundle name="css2">
+    <pop:stylesheet name="/css/bootstrap.min.css" />
+    <pop:stylesheet name="/fonts/gotham/gotham-light/font.css" />
+    <pop:stylesheet name="/fonts/gotham/gotham-book/font.css" />
+    <pop:stylesheet name="/css/customisations.css" />
+  </pop:bundle>
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/js/swiper.min.js"></script>
+
+</head>
+  
+  <body> 
+  
+    <pop:include template="includes/_partials/pages/home/main" />
+      
+    <pop:include template="includes/_partials/scripts/main" />
+  </body>
+</html>
