e11f46c2d60ad51868e42f1f6863a57ddc045397
WHS-Techs <ohmy@whs.ac.nz>
2019-03-29 10:58:01 +1300

    css

--- source/_layouts/home.blade.php
+++ source/_layouts/home.blade.php
@@ -15,6 +15,7 @@
   <link rel="stylesheet" href="/assets/fonts/gotham/gotham-light/font.css" />
   <link rel="stylesheet" href="/assets/fonts/gotham/gotham-book/font.css" />
   <link rel="stylesheet" href="/assets/css/customisations.css">
+  <link rel="stylesheet" href="/css/main.css">
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/js/swiper.min.js"></script>
