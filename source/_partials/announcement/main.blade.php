6895359579d442c8d4628fb6ee753ef6698ca603
WHS-Techs <ohmy@whs.ac.nz>
2019-03-29 16:20:29 +1300

    announcement p

--- source/_partials/announcement/main.blade.php
+++ source/_partials/announcement/main.blade.php
@@ -7,7 +7,9 @@
   <div class="decorated wrap wrap--notification <pop:emergency>emergency</pop:emergency>" style="padding: 1em 0; position: relative; z-index: 99; box-shadow: 0 8px 6px -6px #111">
     <div class="row column" style="display: flex; align-items:center; justify-content: center; text-align: center;flex-wrap: wrap">
 
-      An announcement that is happening
+    <p class="text-center">
+        An announcement that is happening
+    </p>
       <a class="btn" href="<pop:permalink />">Read the full announcement</a>
     </div>
   </div>
