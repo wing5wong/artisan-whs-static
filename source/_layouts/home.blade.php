127c8eb41e45c6bee524c0b3c9eb34ba7db1371c
WHS-Techs <ohmy@whs.ac.nz>
2019-03-29 14:02:34 +1300

    asset helper

--- source/_layouts/home.blade.php
+++ source/_layouts/home.blade.php
@@ -15,7 +15,7 @@
   <link rel="stylesheet" href="/assets/fonts/gotham/gotham-light/font.css" />
   <link rel="stylesheet" href="/assets/fonts/gotham/gotham-book/font.css" />
   <link rel="stylesheet" href="/assets/css/customisations.css">
-  <link rel="stylesheet" href="/assets/build/css/main.css">
+  {{ asset(mix('/css/main.css')) }}
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/js/swiper.min.js"></script>
