c61e810d8c1143e0e62593a2281d917eeb1e5c44
WHS-Techs <ohmy@whs.ac.nz>
2019-03-29 13:47:02 +1300

    process again

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,5 @@ mix.webpackConfig({
 mix.js('source/_assets/js/main.js', 'js')
     .sass('source/_assets/sass/main.scss', 'css')
     .options({
-        processCssUrls: false
+        processCssUrls: true
     }).version();
