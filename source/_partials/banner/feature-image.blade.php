b5c9d59c97a8c706a45a00503e0d7fcd62597e37
WHS-Techs <ohmy@whs.ac.nz>
2019-03-29 14:37:10 +1300

    fix feature image

--- source/_partials/banner/feature-image.blade.php
+++ source/_partials/banner/feature-image.blade.php
@@ -1,3 +1,3 @@
 <div class="row no-gutters">
-    <div class="col p-5 d-none d-md-block bg-attached" style="min-height:45vh; background-image: url(/images/feature_image.jpg)"></div>
+    <div class="col p-5 d-none d-md-block bg-attached feature-image-banner" ></div>
 </div>
