59a270a5b908b5fa3618d8dae9b6920b511ec116
WHS-Techs <ohmy@whs.ac.nz>
2019-03-29 15:59:40 +1300

    standard template

---
+++ source/_layouts/standard.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<body> 
+
+
+<div class="houses">
+  <div class="container pt-5">
+    <div class="row">
+      <div class="col">
+
+        @yield('content')
+        
+      </div>
+    </div>
+  </div>
+</div>
+ 
+  
+</body>
+</html>
---
+++ source/about-whs.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.standard')
+
+
+@section('content')
+    This is the about page
+@endsection
