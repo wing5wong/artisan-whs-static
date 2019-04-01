9e7ea36416c0d1c7205d6ae4ca23599bf0c7102a
WHS-Techs <ohmy@whs.ac.nz>
2019-04-02 10:55:34 +1300

    post standard

--- source/_layouts/standard.blade.php
+++ source/_layouts/standard.blade.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
-  <meta content="" name="description">
-  <meta content="" name="author">
-  <link href="../../../../favicon.ico" rel="icon">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>
+        @yield('title')
+        {{ !empty($__env->yieldContent('title')) ? ' | ' : '' }}
+        {{ $page->site->title }}
+    </title>
 
-  <title>
-      @yield('title')
-      {{ !empty($__env->yieldContent('title')) ? ' | ' : '' }}
-      {{ $page->site->title }}
-  </title>
+    @include('_partials.head.favicon')
+    @include('_partials.head.meta')
+    @include('_partials.cms.identity_widget')
 
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/css/swiper.min.css" rel="stylesheet" />
-  <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/assets/fonts/gotham/gotham-light/font.css" />
-  <link rel="stylesheet" href="/assets/fonts/gotham/gotham-book/font.css" />
-  <link rel="stylesheet" href="/assets/css/customisations.css">
-  <link rel="stylesheet" href="{{mix('/css/main.css', 'assets/build')}}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/css/swiper.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/fonts/gotham/gotham-light/font.css" />
+    <link rel="stylesheet" href="/assets/fonts/gotham/gotham-book/font.css" />
+    <link rel="stylesheet" href="/assets/css/customisations.css">
+    <link rel="stylesheet" href="{{mix('/css/main.css', 'assets/build')}}">
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/js/swiper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/js/swiper.min.js"></script>
 
 </head>
 
