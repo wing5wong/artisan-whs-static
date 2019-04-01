679c9b1d8baa5a973d3961225a6a9b726ad247c5
WHS-Techs <ohmy@whs.ac.nz>
2019-04-02 11:21:28 +1300

    404

--- source/404.blade.php
+++ source/404.blade.php
@@ -9,5 +9,5 @@ permalink: 404.html
 @section('content')
     <h1>404 Error</h1>
 
-    <p>The page you were trying to access could not be found.</p>
+    <img src="https://res.cloudinary.com/whanganuihigh/image/upload/v1554157248/404.jpg" alt="">
 @endsection
