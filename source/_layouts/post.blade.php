6df6b603662e93349571cd9b927ae661ca68661f
WHS-Techs <ohmy@whs.ac.nz>
2019-04-02 12:19:35 +1300

    decorate h1

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -3,7 +3,7 @@
 @section('title', $page->title) 
 
 @section('content')
-    <h1>{{ $page->title }}</h1>
+    <h1 class="decorated">{{ $page->title }}</h1>
 
     {{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}} @if ($page->image)
     <!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
