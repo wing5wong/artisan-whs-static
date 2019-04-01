49d3542401352c4f0466a647f6648dc9faa7a901
WHS-Techs <ohmy@whs.ac.nz>
2019-04-02 11:33:23 +1300

    config.yaml update

--- source/404.blade.php
+++ source/404.blade.php
@@ -7,7 +7,7 @@ permalink: 404.html
 @section('title', 'Page not found')
 
 @section('content')
-    <h1>404 Error</h1>
+    <h1>404 Error - Schoolbag not found</h1>
 
-    <img src="https://res.cloudinary.com/whanganuihigh/image/upload/v1554157248/404.jpg" alt="">
+    <img src="https://res.cloudinary.com/whanganuihigh/image/upload/v1554157248/404.jpg" alt="" style="max-width:100%">
 @endsection
