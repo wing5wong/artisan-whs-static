92e192ba0fca127c78d6410fa9b8371161634b24
WHS-Techs <ohmy@whs.ac.nz>
2019-04-01 15:14:13 +1300

    size

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -8,7 +8,7 @@
     {{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}}
     @if ($page->image)
         <!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
-        <img src="{{ $page->image }}" style="object-fit: cover; height: 250px; width: 100%;">
+        <img src="{{ $page->image }}" style="object-fit: cover;width: 100%;">
     @endif
 
     <p>
