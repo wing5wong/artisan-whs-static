44528ffc5684611ec5399f254c44ab8ebd0e3d79
WHS-Techs <ohmy@whs.ac.nz>
2019-04-02 14:30:00 +1300

    parents page

--- source/for-parents.blade.php
+++ source/for-parents.blade.php
@@ -1,6 +1,19 @@
 @extends('_layouts.standard')
 
 
+@section('title', 'Posts')
+
 @section('content')
-    This is the for parents page
+    <h1>Posts</h1>
+
+    <ul>
+        @forelse ($parents->sortBy('title') as $post)
+            <li>
+                <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
+                <small>{{ date('M j, Y', $post->date) }}</small>
+            </li>
+        @empty
+            <p>No posts to show.</p>
+        @endforelse
+    </ul>
 @endsection
