e4a783887eef0b1d6b22c001ede63903ffbcabbc
WHS-Techs <ohmy@whs.ac.nz>
2019-04-03 10:49:15 +1300

    fix parent urls

--- source/_partials/nav/main.blade.php
+++ source/_partials/nav/main.blade.php
@@ -42,14 +42,14 @@
                 {{$values[0]}}
               </a> @if($$key)
           <div class="dropdown-menu">
-            <a class="dropdown-item" href="/about-whs">About WHS</a> @foreach($$key as $page)
+            <a class="dropdown-item" href="{{$values[1]}}">{{$values[0]}}</a> @foreach($$key as $page)
             <a class="dropdown-item" href="{{$page->getPath()}}">{{$page->title}}</a> @endforeach
           </div>
           @endif
         </li>
 
         @endforeach
-        
+
         <li class="nav-item dropdown px-3">
           <a class="mb-0 p-2 nav-link dropdown-toggle nav-item" href="/curriculum" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Curriculum
