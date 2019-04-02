ca43708b295bb0b77ee711a4c938cfdb632a6d63
WHS-Techs <ohmy@whs.ac.nz>
2019-04-03 10:55:31 +1300

    commas

--- source/_partials/nav/main.blade.php
+++ source/_partials/nav/main.blade.php
@@ -35,8 +35,8 @@
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav w-100 justify-content-center">
 
-        @foreach( [ 'about'=>["About WHS","/about-whs"], "for_parents"=>["For Parents", "/for-parents"] 'curriculum'=>["Curriculum","/curriculum"],
-        "news"=>["News", "/news"] "international"=>["International", "/international"] ] as $key=>$values)
+        @foreach( [ 'about'=>["About WHS","/about-whs"], "for_parents"=>["For Parents", "/for-parents"], 'curriculum'=>["Curriculum","/curriculum"],
+        "news"=>["News", "/news"], "international"=>["International", "/international"] ] as $key=>$values)
 
         <li class="nav-item dropdown px-3">
           <a class="mb-0 p-2 nav-link dropdown-toggle nav-item" href="{{$values[1]}}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
