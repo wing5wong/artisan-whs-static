a8d32018d118e1edf553bcd63c646de770546ab7
WHS-Techs <ohmy@whs.ac.nz>
2019-04-04 09:51:22 +1300

    policies

--- source/policies.blade.php
+++ source/policies.blade.php
@@ -1,3 +1,15 @@
 @foreach($policies as $page)
 {{$page->title}}
+    @foreach($page->policyAreas as $area)
+    {{$area->title}}
+        @foreach($area->policies as $policy)
+        {{$policy->title}}
+        
+
+
+        @endforeach
+
+
+    @endforeach
+
 @endforeach
