79db96ece0e01b4c5647c0909befca2aa1ce0e5e
WHS-Techs <ohmy@whs.ac.nz>
2019-04-04 10:20:37 +1300

    asdasd

--- source/policies.blade.php
+++ source/policies.blade.php
@@ -1,7 +1,7 @@
 @foreach($policies as $page) {{$page->title}}
 <ul>{{$page->title}}
     @foreach($page->policyAreas as $area)
-    <li>{{$area->title}}
+    <li>{{$area->title}}-{{$area->policyArea}}-{{$area->policyArea->title}}
         <ul>
             @foreach($area->policies as $policy)
             <li>
