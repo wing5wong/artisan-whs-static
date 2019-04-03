b8ef1c81954b00f2cb90c42bb135f539d7cfbbe8
WHS-Techs <ohmy@whs.ac.nz>
2019-04-03 14:24:04 +1300

    about page

--- source/about-whs.blade.php
+++ source/about-whs.blade.php
@@ -1,6 +1,19 @@
 @extends('_layouts.standard')
 
 
+@section('title', 'Posts')
+
 @section('content')
-    This is the about page
+    <h1>Posts</h1>
+
+    <ul>
+        @forelse ($about->sortBy('title') as $post)
+            <li>
+                <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
+                <small>{{ date('M j, Y', $post->date) }}</small>
+            </li>
+        @empty
+            <p>No posts to show.</p>
+        @endforelse
+    </ul>
 @endsection
