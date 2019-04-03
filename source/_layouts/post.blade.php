f95575f46ee63b5793aa6e5a7f1f3a16c070f979
WHS-Techs <ohmy@whs.ac.nz>
2019-04-03 16:00:57 +1300

    last reviewed

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -3,14 +3,17 @@
 @section('title', $page->title) 
 
 @section('content')
-    <h1 class="decorated">{{ $page->title }}</h1>
+    <h1 class="decorated py-3 mb-4">{{ $page->title }}</h1>
 
     {{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}} @if ($page->image)
     <!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
     <img src="{{ $page->image }}" style="object-fit: cover;width: 100%;"> @endif @yield('postContent')
 
+
+    <hr>
+
     <p>
-        <strong>{{ date('F j, Y', $page->date) }}</strong><br> @foreach ($page->tags as $tag)
+        <strong>Last Reviewed: {{ date('F j, Y', $page->date) }}</strong><br> @foreach ($page->tags as $tag)
         <a href="/tags/{{ $tag }}">{{ $tag }}</a> {{ $loop->last ? '' : '-' }} @endforeach
     </p>
 
