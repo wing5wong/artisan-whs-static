b05456e53346e287ed9b988910480a82eddeb2d5
WHS-Techs <ohmy@whs.ac.nz>
2019-04-04 12:03:20 +1300

    this way?

--- source/policies.blade.php
+++ source/policies.blade.php
@@ -3,11 +3,11 @@
 @foreach($policies as $policy) {{$policy->title}}
 {{ var_dump($policy->policyAreas)}}
     @foreach($policy->policyAreas as $area)
-    <li>{{$area->title}}-{{$area->policyArea}}-{{$area->policyArea->title}}
+    <li>{{$area["title"]}}-{{$area["policyArea"]}}-{{$area->["policyArea"]["title"]}}
         <ul>
-            @foreach($area->policies as $policy)
+            @foreach($area["policies"] as $policy)
             <li>
-                {{$policy->title}}
+                {{$policy["title"]}}
 
             </li>
 
