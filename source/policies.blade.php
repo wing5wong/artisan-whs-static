c655013fd7d78060829bf741e6c3dd5694b436fb
WHS-Techs <ohmy@whs.ac.nz>
2019-04-04 12:04:53 +1300

    arrow

--- source/policies.blade.php
+++ source/policies.blade.php
@@ -3,7 +3,7 @@
 @foreach($policies as $policy) {{$policy->title}}
 {{ var_dump($policy->policyAreas)}}
     @foreach($policy->policyAreas as $area)
-    <li>{{$area["title"]}}-{{$area["policyArea"]}}-{{$area->["policyArea"]["title"]}}
+    <li>{{$area["title"]}}-{{$area["policyArea"]}}-{{$area["policyArea"]["title"]}}
         <ul>
             @foreach($area["policies"] as $policy)
             <li>
