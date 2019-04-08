cbb942c2c8fd4382c6fbfa1c5618e3331db85ade
WHS-Techs <ohmy@whs.ac.nz>
2019-04-08 15:15:22 +1200

    this

--- source/about-whs.blade.php
+++ source/about-whs.blade.php
@@ -73,7 +73,7 @@
 
 
     <div class="row no-gutters houses">
-        @foreach($about->get(["head-prefects","staff","board-of-trustees","working-at-whs"]) as $area)
+        @foreach([$about->get("head-prefects"),$about->get("staff"),$about->get("board-of-trustees"),$about->get("working-at-whs")]) as $area)
         <div class="col p-5 bg-green text-center text-yellow d-flex flex-column justify-content-between align-items-center">
             <h2 class="mb-0">{{$area["title"]}}</h2>
             <a class="btn btn-outline-light mt-5" href="<pop:permalink />">More information</a>
