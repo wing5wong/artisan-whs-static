3afbd992b5b46a4358c06b69a68b650eab68d276
WHS-Techs <ohmy@whs.ac.nz>
2019-04-08 15:51:17 +1200

    dashes

--- source/about-whs.blade.php
+++ source/about-whs.blade.php
@@ -73,7 +73,7 @@
 
 
     <div class="row no-gutters houses">
-        @foreach($about->only('board_of_trustees')) as $area)
+        @foreach($about->only('board-of-trustees')) as $area)
         <div class="col p-5 bg-green text-center text-yellow d-flex flex-column justify-content-between align-items-center">
             <h2 class="mb-0">{{$area["title"]}}</h2>
             <a class="btn btn-outline-light mt-5" href="<pop:permalink />">More information</a>
