11b2a4e4c304fc0ea52d7c2be501bf20bf36e2b5
Sean Anderson <wing5wong@hotmail.com>
2019-04-12 14:24:49 +1200

    no erray

--- config.php
+++ config.php
@@ -43,7 +43,7 @@ return [
             'comments' => true,
             'tags' => [],
         ],
-        'board_members' => [],
+        'board_members',
         'curriculum' => [
             'path' => 'curriculum/{filename}',
             'sort' => '-date',
