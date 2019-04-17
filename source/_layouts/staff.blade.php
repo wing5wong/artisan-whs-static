0f0b4b0fab9e1ad7413749ae867b0c56f065f8f4
WHS-Techs <ohmy@whs.ac.nz>
2019-04-18 10:14:39 +1200

    gotham and media library full path

--- source/_layouts/staff.blade.php
+++ source/_layouts/staff.blade.php
@@ -7,14 +7,34 @@
 <!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
 <img src="{{ $page->image }}" style="object-fit: cover;width: 100%;"> @endif @yield('postContent')
 
+<h2 class="decorated">Senior Leadership Team</h2>
 
 <?php
 foreach([
+    "Principal",
+    "Associate Principal",
+    "Deputy Principal"] as $dept){
+    $slt = $staff->filter(function($s) use ($dept){
+        return in_array($dept,$s->departments);
+    });
+
+    foreach($slt as $person){
+        ?>
+    <img src="{{$person->image}}" alt="">
+    <h3>{{$person->title}}</h3>
+    <h3>{{$person->position}}</h3>
+
+    <?php
+    }
+}
+?>
+
+        <hr>
+        <h2 class="decorated">Departments</h2>
+        <?php
+foreach([
 "Art",
-"Associate Principal",
-"Board of Trustees",
 "Deans",
-"Deputy Principal",
 "Digital Technology",
 "English",
 "Faculty Heads",
@@ -26,7 +46,6 @@ foreach([
 "Librarians",
 "Mathematics",
 "Physical Education and Health",
-"Principal",
 "Science",
 "Social Sciences",
 "Sports",
@@ -34,7 +53,8 @@ foreach([
 "Support and Ancilliary",
 "Te Atawhai (Special Needs)",
 "Technology",
-"Vocational Studies / Gateway"
+"Vocational Studies / Gateway",
+"Board of Trustees",
 ] as $dept){
 
 $filteredStaff = $staff->filter(function($s) use ($dept){
@@ -56,20 +76,20 @@ echo "<hr />";
 
 
 
-    <p>
-        <strong>{{ date('F j, Y', $page->date) }}</strong><br> @foreach ($page->tags as $tag)
-        <a href="/tags/{{ $tag }}">{{ $tag }}</a> {{ $loop->last ? '' : '-' }} @endforeach
-    </p>
+            <p>
+                <strong>{{ date('F j, Y', $page->date) }}</strong><br> @foreach ($page->tags as $tag)
+                <a href="/tags/{{ $tag }}">{{ $tag }}</a> {{ $loop->last ? '' : '-' }} @endforeach
+            </p>
 
 
-    <blockquote data-phpdate="{{ $page->date }}">
-        <em>WARNING: This post is over a year old. Some of the information this contains may be outdated.</em>
-    </blockquote>
+            <blockquote data-phpdate="{{ $page->date }}">
+                <em>WARNING: This post is over a year old. Some of the information this contains may be outdated.</em>
+            </blockquote>
 
 
 
-    @if ($page->comments)
+            @if ($page->comments)
     @include('_partials.comments') @else
-    <p>Comments are not enabled for this post.</p>
-    @endif
+            <p>Comments are not enabled for this post.</p>
+            @endif
 @endsection
