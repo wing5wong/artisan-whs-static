d36e8eaca8cb74785c488cffe9fa877a0621863a
WHS-Techs <ohmy@whs.ac.nz>
2019-04-29 16:07:19 +1200

    news

--- source/_layouts/news.blade.php
+++ source/_layouts/news.blade.php
@@ -9,34 +9,13 @@
 
 
 <ul>
-@foreach ($pagination->items as $n)
+@foreach ($news as $n)
 <li>
     <h2>{{$n->title}} - {{ date('F j, Y', $n->date) }}</h2>
     {!! $n !!}
 </li>
 @endforeach
 </ul>
-
-@if ($previous = $pagination->previous)
-    <a href="{{ $page->baseUrl }}{{ $pagination->first }}">&lt;&lt;</a>
-    <a href="{{ $page->baseUrl }}{{ $previous }}">&lt;</a>
-@else
-    &lt;&lt; &lt;
-@endif
-
-@foreach ($pagination->pages as $pageNumber => $path)
-    <a href="{{ $page->baseUrl }}{{ $path }}"
-        class="{{ $pagination->currentPage == $pageNumber ? 'selected' : '' }}">
-        {{ $pageNumber }}
-    </a>
-@endforeach
-
-@if ($next = $pagination->next)
-    <a href="{{ $page->baseUrl }}{{ $next }}">&gt;</a>
-    <a href="{{ $page->baseUrl }}{{ $pagination->last }}">&gt;&gt;</a>
-@else
-    &gt; &gt;&gt;
-@endif
 
 <hr>
 
