cb95ba34683a24249d2cc26fa1b45b0f6d04c12f
WHS-Techs <ohmy@whs.ac.nz>
2019-05-07 14:52:28 +1200

    15 per page, only last 5 pages

--- source/news-and-events/news-articles.blade.php
+++ source/news-and-events/news-articles.blade.php
@@ -42,9 +42,11 @@ pagination:
 {{ $pageNumber }}
 </a>
 @else
+    @if( ($pagination->currentPage - $pageNumber <=5) or ($pageNumber - $pagination->currentPage <= 5))
 <a href="{{ $page->baseUrl }}{{ $path }}">
     {{ $pageNumber }}
 </a>
+endif
 @endif
 @endforeach
 
