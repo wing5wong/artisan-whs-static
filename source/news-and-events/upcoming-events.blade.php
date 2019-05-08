340f001b69976b64d3e331e5921c30e8ae05f629
WHS-Techs <ohmy@whs.ac.nz>
2019-05-09 10:39:25 +1200

    events title

--- source/news-and-events/upcoming-events.blade.php
+++ source/news-and-events/upcoming-events.blade.php
@@ -6,7 +6,7 @@ pagination:
 @extends('_layouts.standard')
 @section('title', "Upcoming Events")
 @section('content')
-<h1 class="decorated">Upcoming Events</h1>
+<h1 class="decorated py-3 mb-4">Upcoming Events</h1>
 
 {{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}} @if ($page->image)
 <!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
