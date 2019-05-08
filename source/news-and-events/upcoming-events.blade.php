e24d281f8bc2265b9a154b2b03979b9bbc5c3ad5
WHS-Techs <ohmy@whs.ac.nz>
2019-05-09 10:50:14 +1200

    event card style

--- source/news-and-events/upcoming-events.blade.php
+++ source/news-and-events/upcoming-events.blade.php
@@ -16,19 +16,14 @@ pagination:
 <div class="row mb-5">
 @foreach($pagination->items as $event)
 <div class="col-6">
-    <div class="row">
-        <div class="col-2">
-            <img src="{{ $event->image ?: "https://res.cloudinary.com/whanganuihigh/image/upload/v1554149869/logo_vertical_t.png" }}" style="object-fit: cover;width: 100%;">
-        </div>
-        <div class="col-10">
-            <h2>{{$event->title}}<br>
-                <small>{{ date('F j, Y', $event->date) }} </small>
-            </h2>
-            <div class="row">
-                <div class="col-10">
-                    <a href="{{$event->getPath()}}">Read More</a>
-                </div>
-            </div>
+    <div class="card bg-light mb-3 mb-3" style="max-width: 18rem;">
+        <img class="card-img-top" src="{{ $event->image ?: "https://res.cloudinary.com/whanganuihigh/image/upload/v1554149869/logo_vertical_t.png" }}" style="object-fit: cover;width: 100%;">
+        <div class="card-header">{{ date('F j, Y', $event->date) }}</div>
+        <div class="card-body">
+            <h5 class="card-title">{{$event->title}}</h5>
+            <p class="card-text">  
+            <a href="{{$event->getPath()}}">Event Details</a>
+            </p>
         </div>
     </div>
 </div>
