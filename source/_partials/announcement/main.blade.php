fe8a81be9100d4d1a527f719b522c893550dbc10
WHS-Techs <ohmy@whs.ac.nz>
2019-06-18 16:23:56 +1200

    stuff

--- source/_partials/announcement/main.blade.php
+++ source/_partials/announcement/main.blade.php
@@ -1,18 +1,18 @@
 @if($announcement = $announcements->first())
 <style>
-    .emergency {
-      background: #a41e21;
-      color: #fff
-    }
-  </style>
+  .emergency {
+    background: #a41e21;
+    color: #fff
+  }
+</style>
 
-<div class="decorated wrap wrap--notification {{$announcement->is_emergency ? "emergency" : ''}}" style="padding: 1em 0; position: relative; z-index: 99; box-shadow: 0 8px 6px -6px #111">
-    <div class="row column" style="display: flex; align-items:center; justify-content: center; text-align: center;flex-wrap: wrap">
+<div class="decorated wrap wrap--notification text-center {{$announcement->is_emergency ? "emergency" : ''}}"
+  style="padding: 1em 0; position: relative; z-index: 99; box-shadow: 0 8px 6px -6px #111">
 
-    <h2 class="text-center">
-        {{ $announcement->title }}
-    </h2>
-      <a class="btn btn-light" href="{{$announcement->getPath()}}">Read the full announcement</a>
-    </div>
-  </div>
+  <h2>
+    {{ $announcement->title }}
+  </h2>
+  <a class="btn btn-light" href="{{$announcement->getPath()}}">Read the full announcement</a>
+
+</div>
 @endif
--- source/payments.blade.md
+++ source/payments.blade.md
@@ -1,16 +1,15 @@
 ---
-title: Payments
 extends: _layouts.post
-customPath: ''
+title: Payments
 ---
 Payments can be made by cash, cheque, EFTPOS or creditcard (upon presentation of the card) at Whanganui High School.  Please ensure all cash payments are the correct amount and that all cheques are made payable to Whanganui High School.
 
 Payments can also be made directly to our bank account.  If using this method, please add your student’s school ID number as a reference, plus details if the deposit is for a specific purpose. 
 
 Our bank details are:  
-**Whanganui High School Board of Trustees  
-****ASB Wanganui  
-****123163-0095001-00**
+**Whanganui High School Board of Trustees**
+**ASB Wanganui**
+**123163-0095001-00**
 
 We welcome regular automatic payments to our above-mentioned bank account, for the payment of students’ school accounts.
 
