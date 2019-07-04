87e956d4ef465693a250995e8e8b6ef66c8fe1cd
WHS-Techs <ohmy@whs.ac.nz>
2019-07-04 14:30:42 +1200

    update packages

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -5,9 +5,11 @@
 @section('content')
     <h1 class="decorated py-3 mb-4">{{ $page->title }}</h1>
 
-    {{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}} @if ($page->image)
+    {{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}}
+    @if ($page->image)
     <!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
-    <img src="{{ $page->imageCdn($page->image) }}"  style="object-fit: cover; max-width:100%"> @endif @yield('postContent')
+    <img src="{{ $page->image }}"  style="object-fit: cover; max-width:100%">
+    @endif @yield('postContent')
 
 
  
