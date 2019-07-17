7083cc0fe288b576bdf5dd31d9ea977ca0307714
WHS-Techs <ohmy@whs.ac.nz>
2019-07-18 11:46:38 +1200

    news images

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -15,7 +15,9 @@
 
     @if(is_array($page->image_gallery))
         @foreach($page->image_gallery as $image)
-            <img src="{{$image["image"]}}" alt="{{$image->description}}" title="{{$image->title}}">
+        <a href="{{$image["image"]}}">
+            <img src="{{ str_replace("https://res.cloudinary.com/whanganuihigh/image/upload/","https://res.cloudinary.com/whanganuihigh/image/upload/c_scale,q_80,w_300/",$image["image"])}}" alt="{{$image->description}}" title="{{$image->title}}">
+        </a>
         @endforeach
     @endif
 
