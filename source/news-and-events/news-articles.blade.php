484f4f0927c9fcb426d2e1aad50f2a6be8404ddd
WHS-Techs <ohmy@whs.ac.nz>
2019-07-18 14:10:16 +1200

    only show page date if it exists

--- source/news-and-events/news-articles.blade.php
+++ source/news-and-events/news-articles.blade.php
@@ -19,7 +19,7 @@ pagination:
     </div>
     <div class="col-10">
         <h2>{{$n->title}}<br>
-            <small class="text-muted">{{ date('F j, Y', $n->date) }} </small>
+            @if($page->date)<small class="text-muted">{{ date('F j, Y', $n->date) }} </small>@endif
         </h2>
         <div class="row">
             <div class="col-10">
