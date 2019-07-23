191e13bf4ea478d5a6091110a3aae02060b600d2
WHS-Techs <ohmy@whs.ac.nz>
2019-07-23 15:15:43 +1200

    wat

--- source/news-and-events/upcoming-events.blade.php
+++ source/news-and-events/upcoming-events.blade.php
@@ -8,10 +8,13 @@ perPage: 25
 @section('content')
 <h1 class="decorated py-3 mb-4">Upcoming Events</h1>
 
-{{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}} @if
-($page->image)
+{{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}} 
+@if($page->image)
 <!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
-<img src="{{ $page->image }}" style="object-fit: cover;width: 100%;"> @endif @yield('postContent')
+<img src="{{ $page->image }}" style="object-fit: cover;width: 100%;">
+@endif
+
+@yield('postContent')
 
 
 <div class="row mb-5">
