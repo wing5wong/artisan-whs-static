f52224549bbbdec38d21d87932a8bbfa0e0452ee
WHS-Techs <ohmy@whs.ac.nz>
2019-07-23 15:10:10 +1200

    here... ?

--- source/_layouts/upcoming-events.blade.php
+++ source/_layouts/upcoming-events.blade.php
@@ -8,9 +8,13 @@ pagination:
 @section('content')
 <h1 class="decorated">{{ $page->title }}</h1>
 
-{{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}} @if ($page->image)
+{{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}} 
+@if($page->image)
 <!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
-<img src="{{ $page->image }}" style="object-fit: cover;width: 100%;"> @endif @yield('postContent')
+<img src="{{ $page->image }}" style="object-fit: cover;width: 100%;">
+@endif 
+
+@yield('postContent')
 
 
 <table class="table table-striped table-borderless table-hover">
