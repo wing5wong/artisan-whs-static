fbcba1b6fcafe4690178eb68829d2d11e4e9b8a9
Sean Anderson <wing5wong@hotmail.com>
2019-07-23 20:43:28 +1200

    conditionals

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -3,16 +3,16 @@
 @section('title', $page->title) 
 
 @section('content')
+<main>
     <h1 class="decorated py-3 mb-4">{{ $page->title }}</h1>
 
     {{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}}
     @if ($page->image)
     <!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
-    <a href="{{ $page->image }}" title="{{$page->image_title}}" alt="{{$page->image_alt}}" class="featured">
+    <a href="{{ $page->image }}" @if($page->image_title)title="{{$page->image_title}}"@endif @if($page->image_alt)alt="{{$page->image_alt}}"@endif class="featured">
     <img src="{{ $page->image }}"  style="object-fit: cover; max-width:100%; display: block;">
 </a>
     @endif
-    
     @yield('postContent')
 
     @if(is_array($page->image_gallery))
@@ -36,6 +36,7 @@ a.featured[title]:after {
     }
 
      </style>
+     </main>
     @include('_partials.lastReviewed')
     
 @endsection
