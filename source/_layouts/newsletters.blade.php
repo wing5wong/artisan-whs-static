6e790f97319d009b25b41084344ac07ce2987156
WHS-Techs <ohmy@whs.ac.nz>
2019-07-23 15:00:45 +1200

    on one line....

--- source/_layouts/newsletters.blade.php
+++ source/_layouts/newsletters.blade.php
@@ -3,9 +3,14 @@
 @section('content')
 <h1 class="decorated py-3 mb-4">{{ $page->title }}</h1>
 
-{{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}} @if ($page->image)
+{{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}} 
+@if($page->image)
 <!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
-<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover;width: 100%;"> @endif @yield('postContent')
+<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover;width: 100%;">
+@endif
+
+
+@yield('postContent')
 
 
 
