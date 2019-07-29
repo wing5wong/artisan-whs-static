45d266a88f248d813fde7a2168b070a70b6d4879
WHS-Techs <ohmy@whs.ac.nz>
2019-07-30 11:51:09 +1200

    add missing function

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -53,30 +53,36 @@ a.featured[title]:after {
     
 
     <script>
+        function addClass(el, className) {
+            if (el.classList) el.classList.add(className);
+            else if (!hasClass(el, className)) el.className += ' ' + className;
+        }
+
+
         function wrapAll(nodes, wrapper) {
-    // Cache the current parent and previous sibling of the first node.
-    var parent = nodes[0].parentNode;
-    var previousSibling = nodes[0].previousSibling;
+            // Cache the current parent and previous sibling of the first node.
+            var parent = nodes[0].parentNode;
+            var previousSibling = nodes[0].previousSibling;
 
-    // Place each node in wrapper.
-    //  - If nodes is an array, we must increment the index we grab from 
-    //    after each loop.
-    //  - If nodes is a NodeList, each node is automatically removed from 
-    //    the NodeList when it is removed from its parent with appendChild.
-    for (var i = 0; nodes.length - i; wrapper.firstChild === nodes[0] && i++) {
-        wrapper.setAttribute('title', nodes[i].getAttribute('title'))
-        wrapper.setAttribute('href', nodes[i].getAttribute('href'))
-        wrapper.addClass('featured')
-        wrapper.appendChild(nodes[i]);
-    }
+            // Place each node in wrapper.
+            //  - If nodes is an array, we must increment the index we grab from 
+            //    after each loop.
+            //  - If nodes is a NodeList, each node is automatically removed from 
+            //    the NodeList when it is removed from its parent with appendChild.
+            for (var i = 0; nodes.length - i; wrapper.firstChild === nodes[0] && i++) {
+                wrapper.setAttribute('title', nodes[i].getAttribute('title'))
+                wrapper.setAttribute('href', nodes[i].getAttribute('href'))
+                addClass(wrapper, 'featured')
+                wrapper.appendChild(nodes[i]);
+            }
 
-    // Place the wrapper just after the cached previousSibling,
-    // or if that is null, just before the first child.
-    var nextSibling = previousSibling ? previousSibling.nextSibling : parent.firstChild;
-    parent.insertBefore(wrapper, nextSibling);
+            // Place the wrapper just after the cached previousSibling,
+            // or if that is null, just before the first child.
+            var nextSibling = previousSibling ? previousSibling.nextSibling : parent.firstChild;
+            parent.insertBefore(wrapper, nextSibling);
 
-    return wrapper;
-}
+            return wrapper;
+        }
 
         var nodes = document.querySelectorAll('main img:not(.featured-image)');
         var wrapper = document.createElement('a');
