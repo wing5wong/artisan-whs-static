2f78fce87a9767c6c8f8354c6d5b12c94e406994
WHS-Techs <ohmy@whs.ac.nz>
2019-07-31 16:25:41 +1200

    sort by positions

--- source/_layouts/staff.blade.php
+++ source/_layouts/staff.blade.php
@@ -75,6 +75,9 @@ foreach([
                 ->filter(function($s) use ($theDept){
                         return  !(in_array($s->title, $theDept->hofs) or in_array($s->title, $theDept->ahofs));
                 })
+                ->sortByDesc(function($st){
+                    return $st->position;
+                });
                 ->sortBy(function($st){
                     return array_reverse(explode(" ", $st->title));
                 });
