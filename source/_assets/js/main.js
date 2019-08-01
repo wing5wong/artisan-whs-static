56f822aca15c184fce103f35f4d09a24a3c4f2c2
WHS-Techs <ohmy@whs.ac.nz>
2019-08-02 09:06:52 +1200

    move js to main.js

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -17,3 +17,49 @@ if (outdated) {
         outdated.style.display = 'none';
     }
 }
+
+
+
+
+function addClass(el, className) {
+    if (el.classList) el.classList.add(className);
+    else if (!hasClass(el, className)) el.className += ' ' + className;
+}
+
+
+// add table class to all tables
+var tables = document.querySelectorAll('table');
+for (var i = 0; tables.length - i; i++) {
+    addClass(table, "table table-striped table-borderless table-hover")
+}
+
+
+// wrap image nodes for title/captions
+function wrapAll(nodes, wrapper) {
+    // Cache the current parent and previous sibling of the first node.
+    var parent = nodes[0].parentNode;
+    var previousSibling = nodes[0].previousSibling;
+
+    // Place each node in wrapper.
+    //  - If nodes is an array, we must increment the index we grab from 
+    //    after each loop.
+    //  - If nodes is a NodeList, each node is automatically removed from 
+    //    the NodeList when it is removed from its parent with appendChild.
+    for (var i = 0; nodes.length - i; wrapper.firstChild === nodes[0] && i++) {
+        wrapper.setAttribute('title', nodes[i].getAttribute('title'))
+        wrapper.setAttribute('href', nodes[i].getAttribute('src'))
+        addClass(wrapper, 'featured')
+        wrapper.appendChild(nodes[i]);
+    }
+
+    // Place the wrapper just after the cached previousSibling,
+    // or if that is null, just before the first child.
+    var nextSibling = previousSibling ? previousSibling.nextSibling : parent.firstChild;
+    parent.insertBefore(wrapper, nextSibling);
+
+    return wrapper;
+}
+
+var nodes = document.querySelectorAll('main img:not(.featured-image)');
+var wrapper = document.createElement('a');
+wrapAll(nodes, wrapper)
