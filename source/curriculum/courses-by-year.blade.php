2ccc83bfad8d1858744a65b00f46e4c51a0ba097
WHS-Techs <ohmy@whs.ac.nz>
2019-08-07 15:15:15 +1200

    courses by year take 2

--- source/curriculum/courses-by-year.blade.php
+++ source/curriculum/courses-by-year.blade.php
@@ -5,6 +5,50 @@
 @section('content')
 <h1 class="decorated py-3 mb-4">{{ $page->title }} - {{ $page->name }}</h1>
 
+@php
+$levels = [
+    9 => "Year 9",
+    10 => "Year 10",
+    11 => "Level 1",
+    12 => "Level 2",
+    13 => "Level 3"
+];
+@endphp
+
+@foreach($levels as $level=>$levelTitle)
+    <details>
+        <summary>
+            <h2 class="decorated d-table my-5">{{ $levelTitle }}
+        </summary>
+        @foreach($faculties as $faculty)
+
+            <details>
+                <summary>
+                    <h2 class="decorated d-table my-5">{{ $faculty->title }}
+                </summary>
+                @foreach($subject_areas->where('faculty', $faculty->title)->sortBy('title') as $subject )
+                    <div class="col col-md-6 col-lg-6">
+                    <details open>
+                            <summary><h5 class="d-table">{{ $subject->title }}</h5></summary>
+                        <ul>
+                        @foreach($courses->filter(function($course) use ($level){
+                            return $course->year == $level and $course->subject_area == $subject->title;
+                        }) as $course)
+                        <li>
+                            <a href="{{$course->getPath()}}">{{$course->course_level}} - {{ $course->name }}</a>
+                        </li>
+                        @endforeach
+                        </ul>
+                    </details>
+                    </div>
+                @endforeach
+            </details>
+        @endforeach
+    </details>
+@endforeach
+
+
+
 
 @foreach($courses->groupBy(['year','subject_area']) as $year=>$sa)
 <details>
