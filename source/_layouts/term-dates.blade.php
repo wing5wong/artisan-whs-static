1fa097bf45b01c22b14a8bafa9f7cd1014001bdf
WHS-Techs <ohmy@whs.ac.nz>
2019-08-07 14:59:28 +1200

    term dates as partial

--- source/_layouts/term-dates.blade.php
+++ source/_layouts/term-dates.blade.php
@@ -7,36 +7,8 @@
 <!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
 <img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover;width: 100%;"> @endif @yield('postContent')
 
-@foreach($term_dates as $td)
-<h2 class="d-table decorated mb-4">{{$td->title}}</h2>
 
-{!! $td !!}
-
-@if($td->title != "Public Holidays")
-<h3>Start Dates</h3>
-@endif
-<table>
-    @foreach($td->start_dates as $date)
-        <tr>
-            <td width="30%">{{ date('F j, Y', $date["date"]) }}</td>
-            <td>{{ $date["body"] }}</td>
-        </tr>
-    @endforeach
-</table>
-
-
-@if($td->title != "Public Holidays")
-<h3>End Dates</h3>
-<table>
-    @foreach($td->end_dates as $date)
-        <tr>
-            <td width="30%">{{ date('F j, Y', $date["date"]) }}</td>
-            <td>{{ $date["body"] }}</td>
-        </tr>
-    @endforeach
-</table>
-@endif
-@endforeach
+@include('_partials.term-dates')
 
 @include('_partials.lastReviewed')
 
---
+++ source/_partials/term-dates.blade.php
@@ -0,0 +1,31 @@
+
+@foreach($term_dates as $td)
+<h2 class="d-table decorated mb-4">{{$td->title}}</h2>
+
+{!! $td !!}
+
+@if($td->title != "Public Holidays")
+<h3>Start Dates</h3>
+@endif
+<table>
+    @foreach($td->start_dates as $date)
+        <tr>
+            <td width="30%">{{ date('F j, Y', $date["date"]) }}</td>
+            <td>{{ $date["body"] }}</td>
+        </tr>
+    @endforeach
+</table>
+
+
+@if($td->title != "Public Holidays")
+<h3>End Dates</h3>
+<table>
+    @foreach($td->end_dates as $date)
+        <tr>
+            <td width="30%">{{ date('F j, Y', $date["date"]) }}</td>
+            <td>{{ $date["body"] }}</td>
+        </tr>
+    @endforeach
+</table>
+@endif
+@endforeach
