dd024709065307b0b8a37233673f0755c4bdd466
WHS-Techs <ohmy@whs.ac.nz>
2019-08-12 15:35:52 +1200

    hm

--- config.php
+++ config.php
@@ -81,15 +81,15 @@ return [
             return !( $deptHofs->contains($s->title) or $deptAHofs->contains($s->title));
         })
         ->sort(function($st, $other) use ($departmentToFind){
-            $stPosition = collect($st->positions ?? [])->firstWhere('title', $departmentToFind)->title;
-            $otherPosition = collect($other->positions ?? [])->firstWhere('title', $departmentToFind)->title;
+            $stPosition = collect($st->positions ?? [])->firstWhere('title', $departmentToFind)->title ?? "ZZZZZZZZZZZZZZZZZZZZZZZ";
+            $otherPosition = collect($other->positions ?? [])->firstWhere('title', $departmentToFind)->title ?? "ZZZZZZZZZZZZZZZZZZZZZZZ";
             if($stPosition and $otherPosition and explode(" ", $stPosition)[0]  ==  explode(" ", $otherPosition )[0] ){
                 return strcmp(
                     implode(" ", array_reverse(explode(" ", $st->title))) ,
                     implode(" ", array_reverse(explode(" ", $other->title)))
                 );
              }
-             return strcmp( $stPosition ?? "ZZZZZZZZZZZZZZZZZZZZZZZ", $otherPosition ?? "ZZZZZZZZZZZZZZZZZZZZZZZ");
+             return strcmp( $stPosition , $otherPosition );
         });
     },
 
