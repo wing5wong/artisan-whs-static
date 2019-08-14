34ece29e17a7315eae6e70f90fae6ec6e048e426
Sean Anderson <wing5wong@hotmail.com>
2019-08-14 17:13:55 +1200

    Revert "defer scripts"
    This reverts commit 37f8432b76c40f17b93130507a0aa4ba126c76e6.

--- source/_layouts/home.blade.php
+++ source/_layouts/home.blade.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="{{mix('/css/main.css', 'assets/build')}}">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/js/swiper.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/js/swiper.min.js"></script>
 
 </head>
 
