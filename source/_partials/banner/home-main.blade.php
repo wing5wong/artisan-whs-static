124d933025305a0c09ce1c2ecff2ef909e4381a8
WHS-Techs <ohmy@whs.ac.nz>
2019-08-14 16:22:38 +1200

    no space

--- source/_partials/banner/home-main.blade.php
+++ source/_partials/banner/home-main.blade.php
@@ -4,7 +4,7 @@ d-flex flex-column
 justify-content-between align-items-between">
     <picture>
       <source srcset="https://res.cloudinary.com/whanganuihigh/image/upload/v1554149869/logo_vertical_t.png" media="(max-width: 750px)" />
-      <img alt="" class="p-5 site-banner-img img-fluid position-absolute" style="top:0; right: 0; width: 250px;" src="https://res.cloudinary.com/whanganuihigh/image/upload/f_auto, q_auto/v1554149869/logo_vertical_t.png">
+      <img alt="" class="p-5 site-banner-img img-fluid position-absolute" style="top:0; right: 0; width: 250px;" src="https://res.cloudinary.com/whanganuihigh/image/upload/f_auto,q_auto/v1554149869/logo_vertical_t.png">
     </picture>
 
   </div>
