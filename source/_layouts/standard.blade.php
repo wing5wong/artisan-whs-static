25e541d19fcc402febb3d7eb468919bf37ef2e09
WHS-Techs <ohmy@whs.ac.nz>
2019-08-15 11:37:22 +1200

    swiper to main css, fonts back out

--- source/_layouts/standard.blade.php
+++ source/_layouts/standard.blade.php
@@ -22,8 +22,10 @@
     @include('_partials.head.favicon')
     @include('_partials.head.meta')
     @include('_partials.cms.identity_widget')
+    
+    <link href="/assets/fonts/gotham/gotham-light/font.css" rel="stylesheet" />
+<link href="/assets/fonts/gotham/gotham-book/font.css" rel="stylesheet" />
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/css/swiper.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="{{mix('/css/main.css', 'assets/build')}}">
 </head>
 
