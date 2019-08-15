40904d73c22a71305e77c276dc2f8060ac241bb0
Sean Anderson <wing5wong@hotmail.com>
2019-08-15 20:14:20 +1200

    dont need extra analytics

--- source/_partials/footer/scripts.blade.php
+++ source/_partials/footer/scripts.blade.php
@@ -1,3 +1,3 @@
   <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
-  @includeWhen($page->production, '_partials.analytics')
+ {{-- @includeWhen($page->production, '_partials.analytics') --}}
   @include('_partials.cms.identity_redirect')
