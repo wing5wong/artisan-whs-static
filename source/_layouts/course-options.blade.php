229da763a16d34e14483d5bf1f9b19ef1081962b
WHS-Techs <ohmy@whs.ac.nz>
2019-08-16 15:31:50 +1200

    courses in tabs

--- source/_layouts/course-options.blade.php
+++ source/_layouts/course-options.blade.php
@@ -17,6 +17,84 @@ $levels = [
 ];
 @endphp
 
+
+
+
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+
+    @foreach($levels as $level=>$levelTitle)
+    <li class="nav-item">
+        <a class="nav-link active" id="year-{{$level}}-tab" data-toggle="tab" href="#year-{{$level}}" role="tab"
+            aria-controls="home" aria-selected="true">{{ $levelTitle }}</a>
+    </li>
+    @endforeach
+</ul>
+<div class="tab-content" id="myTabContent">
+
+    @foreach($levels as $level=>$levelTitle)
+    <div class="tab-pane fade show @if($loop->first()) active @endif" id="year-{{$level}}" role="tabpanel" aria-labelledby="year-{{$level}}-tab">
+
+        @php
+        $mapped = $page->getTeachingFaculties($faculties)
+        ->map(function($faculty) use ($page, $level, $subject_areas, $courses)
+        {
+        return [
+        'faculty'=> $faculty,
+        'courses' => $page->getFacultyCoursesForLevel($faculty, $subject_areas, $courses, $level),
+        ];
+        })->filter( function($f){
+        return $f['courses']->isNotEmpty();
+        });
+        @endphp
+
+        <div class="row">
+            @foreach($mapped as $faculty)
+            <div class="col-6">
+                <h3 class="d-table mt-3">
+                    {{ $faculty['faculty']->title}}
+                    @if($faculty['faculty']->maori_title)<br><small
+                        class="text-muted">{{$faculty['faculty']->maori_title}}</small>@endif
+                </h3>
+
+
+                @if($page->yearLevelOffersVocationalPathways($level) and count($faculty['faculty']->vocational_pathways
+                ?? []))
+                <ul class="list-inline">
+                    @foreach($faculty['faculty']->vocational_pathways as $vp)
+                    <li class="list-inline-item">
+                        <a href="{{ $page['vp'][$vp]['url']}}" class="text-white px-2 py-1 badge badge-vp-{{$vp}}"
+                            title="{{ $page['vp'][$vp]['name']}}" target="_BLANK">{{$page['vp'][$vp]['code']}}</a>
+                    </li>
+                    @endforeach
+                </ul>
+                @endif
+
+
+                @if($faculty['faculty']->intro)
+                <br>
+                {{ $faculty['faculty']->intro }}
+                @endif
+
+
+
+                <div class="list-group my-4">
+                    @foreach($faculty['courses'] as $course)
+                    <a class="list-group-item list-group-item-action"
+                        href="{{$course->getPath()}}">{{ $course->name }}</a>
+                    @endforeach
+                </div>
+
+            </div>
+            @endforeach
+        </div>
+
+    </div>
+    @endforeach
+
+</div>
+
+
+
 @foreach($levels as $level=>$levelTitle)
 <details class="mb-4">
     <summary>
@@ -24,15 +102,15 @@ $levels = [
     </summary>
     @php
     $mapped = $page->getTeachingFaculties($faculties)
-        ->map(function($faculty) use ($page, $level, $subject_areas, $courses)
-        {
-            return [
-                'faculty'=> $faculty,
-                'courses' => $page->getFacultyCoursesForLevel($faculty, $subject_areas, $courses, $level),
-            ];
-        })->filter( function($f){
-            return $f['courses']->isNotEmpty();
-        });
+    ->map(function($faculty) use ($page, $level, $subject_areas, $courses)
+    {
+    return [
+    'faculty'=> $faculty,
+    'courses' => $page->getFacultyCoursesForLevel($faculty, $subject_areas, $courses, $level),
+    ];
+    })->filter( function($f){
+    return $f['courses']->isNotEmpty();
+    });
     @endphp
 
     <div class="row">
@@ -45,12 +123,13 @@ $levels = [
             </h3>
 
 
-            @if($page->yearLevelOffersVocationalPathways($level) and count($faculty['faculty']->vocational_pathways ??  []))
+            @if($page->yearLevelOffersVocationalPathways($level) and count($faculty['faculty']->vocational_pathways ??
+            []))
             <ul class="list-inline">
                 @foreach($faculty['faculty']->vocational_pathways as $vp)
                 <li class="list-inline-item">
-                    <a href="{{ $page['vp'][$vp]['url']}}" class="text-white px-2 py-1 badge badge-vp-{{$vp}}" title="{{ $page['vp'][$vp]['name']}}"
-                        target="_BLANK">{{$page['vp'][$vp]['code']}}</a>
+                    <a href="{{ $page['vp'][$vp]['url']}}" class="text-white px-2 py-1 badge badge-vp-{{$vp}}"
+                        title="{{ $page['vp'][$vp]['name']}}" target="_BLANK">{{$page['vp'][$vp]['code']}}</a>
                 </li>
                 @endforeach
             </ul>
@@ -66,7 +145,7 @@ $levels = [
 
             <div class="list-group my-4">
                 @foreach($faculty['courses'] as $course)
-                    <a class="list-group-item list-group-item-action" href="{{$course->getPath()}}">{{ $course->name }}</a>
+                <a class="list-group-item list-group-item-action" href="{{$course->getPath()}}">{{ $course->name }}</a>
                 @endforeach
             </div>
 
