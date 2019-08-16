fe08784086daab907843cbffce542215d6e01632
WHS-Techs <ohmy@whs.ac.nz>
2019-08-16 16:30:04 +1200

    use the tabbed version

--- source/_layouts/course-options.blade.php
+++ source/_layouts/course-options.blade.php
@@ -17,9 +17,6 @@ $levels = [
 ];
 @endphp
 
-
-
-
 <ul class="nav nav-tabs" id="myTab" role="tablist">
 
     @foreach($levels as $level=>$levelTitle)
@@ -92,70 +89,5 @@ $levels = [
     @endforeach
 
 </div>
-
-
-
-@foreach($levels as $level=>$levelTitle)
-<details class="mb-4">
-    <summary>
-        <h2 class="decorated d-table">{{ $levelTitle }}
-    </summary>
-    @php
-    $mapped = $page->getTeachingFaculties($faculties)
-    ->map(function($faculty) use ($page, $level, $subject_areas, $courses)
-    {
-    return [
-    'faculty'=> $faculty,
-    'courses' => $page->getFacultyCoursesForLevel($faculty, $subject_areas, $courses, $level),
-    ];
-    })->filter( function($f){
-    return $f['courses']->isNotEmpty();
-    });
-    @endphp
-
-    <div class="row">
-        @foreach($mapped as $faculty)
-        <div class="col-6">
-            <h3 class="d-table mt-3">
-                {{ $faculty['faculty']->title}}
-                @if($faculty['faculty']->maori_title)<br><small
-                    class="text-muted">{{$faculty['faculty']->maori_title}}</small>@endif
-            </h3>
-
-
-            @if($page->yearLevelOffersVocationalPathways($level) and count($faculty['faculty']->vocational_pathways ??
-            []))
-            <ul class="list-inline">
-                @foreach($faculty['faculty']->vocational_pathways as $vp)
-                <li class="list-inline-item">
-                    <a href="{{ $page['vp'][$vp]['url']}}" class="text-white px-2 py-1 badge badge-vp-{{$vp}}"
-                        title="{{ $page['vp'][$vp]['name']}}" target="_BLANK">{{$page['vp'][$vp]['code']}}</a>
-                </li>
-                @endforeach
-            </ul>
-            @endif
-
-
-            @if($faculty['faculty']->intro)
-            <br>
-            {{ $faculty['faculty']->intro }}
-            @endif
-
-
-
-            <div class="list-group my-4">
-                @foreach($faculty['courses'] as $course)
-                <a class="list-group-item list-group-item-action" href="{{$course->getPath()}}">{{ $course->name }}</a>
-                @endforeach
-            </div>
-
-        </div>
-        @endforeach
-    </div>
-
-</details>
-@endforeach
-
-
 
 @endsection
