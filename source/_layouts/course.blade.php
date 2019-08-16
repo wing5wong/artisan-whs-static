95800f5eb24c5129632e76cd3f56eacfdba4e331
WHS-Techs <ohmy@whs.ac.nz>
2019-08-16 12:55:56 +1200

    course leads to use course name

--- source/_layouts/course.blade.php
+++ source/_layouts/course.blade.php
@@ -79,7 +79,7 @@
         <tr>
             <td colspan='4'><strong>Leads To:</strong> <br>
                 @foreach($courses->whereIn('code', $page->leads_to) as $leads)
-                <a href="{{$leads->getPath()}}">{{ $leads->code }}</a>@if(!$loop->last), @endif
+                <a href="{{$leads->getPath()}}">{{ $leads->name }} ({{$leads->level}})</a>@if(!$loop->last), @endif
                 @endforeach
             </td>
         </tr>
