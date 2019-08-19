0c1c6f50f07a57d938e9e2efbfd8ab81001ee2e5
WHS-Techs <ohmy@whs.ac.nz>
2019-08-20 10:21:00 +1200

    update subject area layout

--- source/_layouts/subjectAreas.blade.php
+++ source/_layouts/subjectAreas.blade.php
@@ -1,38 +1,48 @@
-@extends('_layouts.standard') 
+@extends('_layouts.standard')
 
-@section('title', $page->title) 
+@section('title', $page->title)
 
 @section('content')
-    <h1 class="decorated py-3 mb-4">{{ $page->title }}</h1>
+<h1 class="decorated py-3 mb-4">{{ $page->title }}</h1>
 
-    {{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}} @if ($page->image)
-    <!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
-    <img src="{{ $page->imageCdn($page->image) }}"  style="object-fit: cover; max-width:100%"> @endif @yield('postContent')
-
-
-    Courses in the {{ $page->title }} Subject Area:
-
-<ul>
-    @foreach($courses->where('subject_area', $page->title) as $c)
-<li>
-    <a href="{{$c->getPath()}}">{{ $c->name }} ({{$c->course_level}})</a>
-</li>
-    @endforeach
-</ul>
+{{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}}
+@if($page->image)
+<!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
+<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; max-width:100%"> @endif @yield('postContent')
 
 
 
+<div class="row">
 
-    Other Subject Areas in the {{ $page->faculty }} faculty:
+    <div class="col-12 col-lg-6">
 
-<ul>
-    @foreach($subject_areas->where('faculty', $page->faculty) as $other)
-<li>
-        <a href="{{$other->getPath()}}">{{ $other->title }}</a>
-</li>
-    @endforeach
-</ul>
-    
-    @include('_partials.lastReviewed')
+        Courses in the {{ $page->title }} Subject Area:
+
+        <ul class="list-group my-4">
+            @foreach($courses->where('subject_area', $page->title) as $c)
+            <li class="list-group-item list-group-item-action">
+                <a href="{{$c->getPath()}}">{{ $c->name }} ({{$c->course_level}})</a>
+            </li>
+            @endforeach
+        </ul>
+
+    </div>
+    <div class="col-12 col-lg-6">
+
+        Other Subject Areas in the {{ $page->faculty }} faculty:
+
+        <ul class="list-group my-4">
+            @foreach($subject_areas->where('faculty', $page->faculty) as $other)
+            <li class="list-group-item list-group-item-action">
+                <a href="{{$other->getPath()}}">{{ $other->title }}</a>
+            </li>
+            @endforeach
+        </ul>
+
+    </div>
+</div>
+
+
+@include('_partials.lastReviewed')
 
 @endsection
