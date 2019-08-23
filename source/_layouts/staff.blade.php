662bdd3d44ce0eb78400e7f24911cf6edff0aa27
WHS-Techs <ohmy@whs.ac.nz>
2019-08-23 12:59:46 +1200

    SLT layout

--- source/_layouts/staff.blade.php
+++ source/_layouts/staff.blade.php
@@ -1,93 +1,92 @@
-@extends('_layouts.standard') 
-@section('title', $page->title) 
+@extends('_layouts.standard')
+@section('title', $page->title)
 @section('content')
 <h1 class="decorated">{{ $page->title }}</h1>
 
-{{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}} @if ($page->image)
-<!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
-<a href="{{ $page->image }}" @if($page->image_title)title="{{$page->image_title}}"@endif @if($page->image_alt)alt="{{$page->image_alt}}"@endif class="featured">
-        <img src="{{ $page->image }}"  style="object-fit: cover; max-width:100%; display: block;">
-    </a>
+{{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}}
+
+@if($page->image)
+    <!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
+    <a href="{{ $page->image }}" @if($page->image_title)title="{{$page->image_title}}"@endif
+    @if($page->image_alt)alt="{{$page->image_alt}}"@endif class="featured">
+    <img src="{{ $page->image }}" style="object-fit: cover; max-width:100%; display: block;">
+</a>
 @endif
 
 @yield('postContent')
 
 <h2 class="d-inline-block decorated">Senior Leadership Team</h2>
 <div class="row">
-    @foreach(["Principal",  "Associate Principal",   "Deputy Principal"] as $dept)
+    @foreach(["Principal", "Associate Principal", "Deputy Principal"] as $dept)
 
-        @foreach($page->getDepartmentStaff($faculties, $staff, $dept) as $person)
-            <article class="col-sm-12 col-md-6 col-lg-6 p-5">
-                <div class="row">    
-                    <div class="col-12">
-                        <h3>{{$person->title}}</h3>
-                            <p class="lead">{{$person->position}}</p>
-                        </div>
-                        <div class="col-12">
-                            <img src="{{$person->image}}" alt="" width="600" alt="{{$person->title}}" style="max-width: 100%">
-                        </div>
-                    </div>
-                </article>
-        @endforeach
+    @foreach($page->getDepartmentStaff($faculties, $staff, $dept) as $person)
+    <article class="col-sm-12 col-md-6 col-lg-6 p-5">
+
+        {{$person->title}}
+        <img src="{{$person->image}}" alt="" width="600" alt="{{$person->title}}" style="max-width: 100%">
+        <p class="lead">{{$person->position}}</p>
+
+    </article>
+    @endforeach
     @endforeach
 </div>
 @foreach($page->getTeachingFaculties($faculties)
 ->concat($page->getNonTeachingFaculties($faculties))
 ->filter(function($f){ return $f->title !== "Senior Leadership Team";})
- as $dept)
+as $dept)
 
-                @php
-                $filteredStaff = $page->getDepartmentStaff($faculties, $staff, $dept->title);  
-                @endphp
-                @if($filteredStaff->isNotEmpty())
-                <details>
-                    <summary>
-                    <h2 class='d-table decorated mt-5 mb-2'>{{ $dept->title }}</h2>
-                    </summary>
-                    <table class="table table-striped table-borderless table-hover">
-                        @foreach($page->getDepartmentHofs($faculties, $staff, $dept->title) as $member)
-                            <tr>
-                                <td>
-                                    <strong>{{ $member->title }}</strong>
-                                </td>
-                                <td>
-                                        @foreach($page->getStaffMemberPositionsForDepartment($member, $dept->title) as $position)
-                                        <strong>{{ $position["title"] }}</strong>
-                                            @if(!$loop->last), @endif
-                                        @endforeach
-                                </td>
-                            </tr>
-                        @endforeach
-                        @foreach($page->getDepartmentAHofs($faculties, $staff, $dept->title) as $member)
-                            <tr>
-                                <td>
-                                    <strong>{{ $member->title }}</strong>
-                                </td>
-                                <td>
-                                    @foreach($page->getStaffMemberPositionsForDepartment($member, $dept->title) as $position)
-                                    <strong>{{ $position["title"] }}</strong>
-                                        @if(!$loop->last), @endif
-                                    @endforeach
-                                </td>
-                            </tr>
-                        @endforeach
-                        @foreach($filteredStaff as $member)
-                            <tr>
-                                <td>
-                                    {{ $member->title }}
-                                </td>
-                                <td>
-                                    @foreach($page->getStaffMemberPositionsForDepartment($member, $dept->title) as $position)
-                                        {{ $position["title"] }}
-                                    @if(!$loop->last), @endif
+@php
+$filteredStaff = $page->getDepartmentStaff($faculties, $staff, $dept->title);
+@endphp
+@if($filteredStaff->isNotEmpty())
+<details>
+    <summary>
+        <h2 class='d-table decorated mt-5 mb-2'>{{ $dept->title }}</h2>
+    </summary>
+    <table class="table table-striped table-borderless table-hover">
+        @foreach($page->getDepartmentHofs($faculties, $staff, $dept->title) as $member)
+        <tr>
+            <td>
+                <strong>{{ $member->title }}</strong>
+            </td>
+            <td>
+                @foreach($page->getStaffMemberPositionsForDepartment($member, $dept->title) as $position)
+                <strong>{{ $position["title"] }}</strong>
+                @if(!$loop->last), @endif
+                @endforeach
+            </td>
+        </tr>
+        @endforeach
+        @foreach($page->getDepartmentAHofs($faculties, $staff, $dept->title) as $member)
+        <tr>
+            <td>
+                <strong>{{ $member->title }}</strong>
+            </td>
+            <td>
+                @foreach($page->getStaffMemberPositionsForDepartment($member, $dept->title) as $position)
+                <strong>{{ $position["title"] }}</strong>
+                @if(!$loop->last), @endif
+                @endforeach
+            </td>
+        </tr>
+        @endforeach
+        @foreach($filteredStaff as $member)
+        <tr>
+            <td>
+                {{ $member->title }}
+            </td>
+            <td>
+                @foreach($page->getStaffMemberPositionsForDepartment($member, $dept->title) as $position)
+                {{ $position["title"] }}
+                @if(!$loop->last), @endif
 
-                                    @endforeach
-                                </td>
-                            </tr>
-                        @endforeach
-                    </table>
-                </details>
-                @endif
+                @endforeach
+            </td>
+        </tr>
+        @endforeach
+    </table>
+</details>
+@endif
 @endforeach
 @include('_partials.lastReviewed')
 
