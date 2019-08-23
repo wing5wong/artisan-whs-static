f836b4e5fa0ed8ef75b82e18424e02e0a088d2e3
WHS-Techs <ohmy@whs.ac.nz>
2019-08-23 12:50:19 +1200

    stationery lists from school documents

---
+++ source/_layouts/school-information/stationery.blade.php
@@ -0,0 +1,46 @@
+@extends('_layouts.standard') 
+@section('title', $page->title) 
+@section('content')
+<h1 class="decorated py-3 mb-4">{{ $page->title }}</h1>
+
+{{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}} @if ($page->image)
+<!--<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">-->
+<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover;width: 100%;"> @endif
+
+@yield('postContent')
+
+
+@php
+    $lists = $school_documents->filter(function($doc){
+        return strpos($doc->title, "Stationery") >= -1;
+    })->sort(function($a, $b){
+        if(date('Y', $a->date) === date('Y', $b->date)){
+            return -($a->title <=> $b->title);
+        }
+        return -($a->date <=> $b->date);
+    });
+@endphp
+
+<h2>Stationery Lists</h2>
+<table>
+    <tr>
+        <th width="35%">Year</th>
+        <th>Stationery List</th>
+    </tr>
+@foreach($lists as $list)
+    <tr>
+        <td>
+                {{ date('Y', $list->date) }}
+        </td>
+        <td>
+                <a href="{{ $list->file}}">{{ $list->title }}</a>
+        </td>
+    </tr>
+@endforeach
+</table>
+
+@include('_partials.lastReviewed')
+
+
+
+@endsection
