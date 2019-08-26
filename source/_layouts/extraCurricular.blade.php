c0332c0663b0a3e90e15e0384cc8634690577ca8
WHS-Techs <ohmy@whs.ac.nz>
2019-08-27 10:23:20 +1200

    ec activities in table

--- source/_layouts/extraCurricular.blade.php
+++ source/_layouts/extraCurricular.blade.php
@@ -4,11 +4,13 @@
 <h1 class="decorated py-3 mb-4">{{ $page->title }}</h1>
 
 @if ($page->image)
+
 <!--<img src="{{ $page->image }}" class="featured-image" style="object-fit: cover; height: 250px; width: 100%;">-->
 <a href="{{ $page->image }}" @if($page->image_title)title="{{$page->image_title}}"@endif
     @if($page->image_alt)alt="{{$page->image_alt}}"@endif class="featured">
     <img src="{{ $page->image }}" style="object-fit: cover; max-width:100%; display: block;">
 </a>
+
 @endif
 
 @yield('postContent')
@@ -21,11 +23,16 @@
 
     {!! $ec_area !!}
 
-    @foreach( $extracurricular_activities->where('extracurricular_area', $page->title) as $ec_activity
-    )
-    <h3>{{$ec_activity->title}}</h3>
-    {!! $ec_activity !!}
-    @endforeach
+    <table>
+
+        @foreach( $extracurricular_activities->where('extracurricular_area', $page->title) as $ec_activity )
+
+        <tr>
+            <a href="{{$ec->getPath()}}">{{$ec_activity->title}}</a>
+        </tr>
+        @endforeach
+
+    </table>
 
     <a href="{{$ec_area->getPath()}}" class="btn btn-light my-5">See all {{$ec_area->title}} activities.</a>
 
