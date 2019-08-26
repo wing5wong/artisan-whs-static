e877f47713d2ccc827c3e7cf8544e405f217ecbf
WHS-Techs <ohmy@whs.ac.nz>
2019-08-27 10:30:04 +1200

    add headings and person in charge

--- source/_layouts/extraCurricular.blade.php
+++ source/_layouts/extraCurricular.blade.php
@@ -16,13 +16,44 @@
 @yield('postContent')
 
 @foreach($extracurricular_areas as $ec_area)
+@php
+$personInCharge = $staff->firstWhere('title', $ec_area->person_in_charge);
+@endphp
 <details>
     <summary>
         <h2 class="decorated d-table mt-5 mb-2">{{$ec_area->title}}</h2>
     </summary>
 
+    @if($ec_area->image)
+    <img src="{{str_replace("https://res.cloudinary.com/whanganuihigh/image/upload/","https://res.cloudinary.com/whanganuihigh/image/upload/q_auto,f_auto,w_500,h_300,c_lfill,g_auto/", $ec_area->image)}}"
+        srcset="
+            {{str_replace("https://res.cloudinary.com/whanganuihigh/image/upload/","https://res.cloudinary.com/whanganuihigh/image/upload/q_auto,f_auto,w_950,h_300,c_lfill,g_auto/", $ec_area->image)}} 950w,
+            {{str_replace("https://res.cloudinary.com/whanganuihigh/image/upload/","https://res.cloudinary.com/whanganuihigh/image/upload/q_auto,f_auto,w_500,h_300,c_lfill,g_auto/", $ec_area->image)}} 500w
+        " sizes="(min-width: 800px) 950px, 500px" alt="" style="max-width: 100%">
+    @endif
+
+    <h3>Person in charge</h3>
+    <table>
+        <thead>
+            <tr>
+                <td>Name</td>
+                <td>Phone</td>
+                <td>Email</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{ $personInCharge->title ?? '' }}</td>
+                <td>{{ $personInCharge->phone ?? '' }}</td>
+                <td>{{ $personInCharge->mobile ?? '' }}</td>
+            </tr>
+        </tbody>
+    </table>
+
     {!! $ec_area !!}
 
+
+    <h3>Available activities</h3>
     <table>
 
         @foreach( $extracurricular_activities->where('extracurricular_area', $page->title) as $ec_activity )
