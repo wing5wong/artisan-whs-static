20f9d9f01dd455168a1a5d8bd8fbab6a62ff9589
WHS-Techs <ohmy@whs.ac.nz>
2019-08-28 10:05:36 +1200

    index of staff

--- listeners/GenerateIndex.php
+++ listeners/GenerateIndex.php
@@ -5,12 +5,10 @@ class GenerateIndex
 {
     public function handle(Jigsaw $jigsaw)
     {
-        $data = collect($jigsaw->getCollection('posts')->map(function ($page) use ($jigsaw) {
+        $data = collect($jigsaw->getCollection('staff')->map(function ($page) use ($jigsaw) {
             return [
                 'title' => $page->title,
-                'categories' => $page->categories,
                 'link' => rightTrimPath($jigsaw->getConfig('baseUrl')) . $page->getPath(),
-                'snippet' => $page->getExcerpt(),
             ];
         })->values());
         file_put_contents($jigsaw->getDestinationPath() . '/index.json', json_encode($data));
