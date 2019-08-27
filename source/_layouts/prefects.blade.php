5d053dd9716e44ee2063aec5b56bbbce262eb0b2
WHS-Techs <ohmy@whs.ac.nz>
2019-08-27 15:44:41 +1200

    srcset for prefect images

--- source/_layouts/prefects.blade.php
+++ source/_layouts/prefects.blade.php
@@ -15,7 +15,16 @@
 <div class="col-sm-12 col-md-4 col-lg-4 mb-5">
 <h3>{!! $these !!}</h3>
 {!! $people->last()->title !!}
+
 <img src="{{$people->last()->image}}" />
+
+<img src="{{str_replace("https://res.cloudinary.com/whanganuihigh/image/upload/","https://res.cloudinary.com/whanganuihigh/image/upload/q_auto,f_auto,w_320,h_320,c_lfill,g_auto/", $people->last()->image)}}"
+        srcset="
+        {{str_replace("https://res.cloudinary.com/whanganuihigh/image/upload/","https://res.cloudinary.com/whanganuihigh/image/upload/q_auto,f_auto,w_320,h_320,c_lfill,g_auto/", $people->last()->image)}}
+        "
+alt="{{${$these} - ${$people->last()->title} }}" style="max-width: 100%">
+
+
 </div>
 @endforeach
     </div>
