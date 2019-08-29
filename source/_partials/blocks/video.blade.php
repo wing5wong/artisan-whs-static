2ff06e627cc653c7572561098fc108aa39c31f54
WHS-Techs <ohmy@whs.ac.nz>
2019-08-29 15:00:24 +1200

    add example video block

---
+++ source/_partials/blocks/video.blade.php
@@ -0,0 +1,11 @@
+<section class="page-textBlock">
+    <h3>{{ $block['heading'] }}</h3>
+    @if($block["content"])
+      <div class="max-width--330">
+        {!! $block["content"] !!}
+      </div>
+    @endif
+    {{-- @if($block["video"]) -- }}
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/mIBZWYOw4dU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    {{-- @endif --}}
+    </section>
