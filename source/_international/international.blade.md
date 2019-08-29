b15e30fdab26ff5d91938015387e6e9ffdb0a1af
WHS-Techs <ohmy@whs.ac.nz>
2019-08-29 16:29:09 +1200

    fix international

--- source/_international/international.blade.md
+++
@@ -1,4 +0,0 @@
----
-title: International
-extends: _layouts.international
----
--- source/international.blade.php
+++ source/international.blade.php
@@ -1,16 +1,21 @@
-@extends('_layouts.standard') 
-@section('title', "International") 
+@extends('_layouts.standard')
+@section('title', $page->title)
 @section('content')
-<h1 class="decorated py-3 mb-4">International</h1>
+<h1 class="decorated py-3 mb-4">{{ $page->title }}</h1>
+
+@if($page->image)
+<img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover;width: 100%;">
+@endif
 
 
-<div style="height: 0; position: relative; padding-bottom: 56.25%">
-  <iframe style="position: absolute; top:0; left: 0; " width="100%" height="100%" src="https://www.youtube.com/embed/mKJynLxG79k"
-    frameborder="0" allowfullscreen></iframe>
-</div>
-  @include('_international-content')
+@yield('postContent')
 
 
-  @include('_partials.term-dates')
+@foreach($page->blocks as $block)
+    @include('_partials.blocks.' . $block["type"], ['block'=>$block])
+@endforeach
+
+
+@include('_partials.lastReviewed')
 
 @endsection
