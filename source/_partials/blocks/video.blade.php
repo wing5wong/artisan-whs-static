508420767a09560fd35ca3040f4f599f6888e312
Sean Anderson <wing5wong@hotmail.com>
2019-08-30 00:06:58 +1200

    video url

--- source/_partials/blocks/video.blade.php
+++ source/_partials/blocks/video.blade.php
@@ -7,10 +7,10 @@
         {!! $block["content"] !!}
       </div>
     @endif
-    {{-- @if($block["video"]) --}}
+    {{-- @if($block["url"]) --}}
     
 <div style="height: 0; position: relative; padding-bottom: 56.25%">
-  <iframe style="position: absolute; top:0; left: 0; " width="100%" height="100%" src="https://www.youtube.com/embed/{{$block['video']}}"
+  <iframe style="position: absolute; top:0; left: 0; " width="100%" height="100%" src="https://www.youtube.com/embed/{{$block['url']}}"
     frameborder="0" allowfullscreen></iframe>
 </div>
     {{-- @endif --}}
