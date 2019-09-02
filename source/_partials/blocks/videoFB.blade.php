d2eb54821bd7afe5fb14fe87345ae5a3c3269917
WHS-Techs <ohmy@whs.ac.nz>
2019-09-02 15:34:14 +1200

    add facebook video and page builder to extra curr activities

---
+++ source/_partials/blocks/videoFB.blade.php
@@ -0,0 +1,18 @@
+
+    @if($block['heading'])
+    <h3>{{ $block['heading'] }}</h3>
+    @endif
+    @if($block["content"])
+      <div class="max-width--330">
+        {!! $block["content"] !!}
+      </div>
+    @endif
+    {{-- @if($block["url"]) --}}
+    
+<div style="height: 0; position: relative; padding-bottom: 56.25%">
+        <iframe style="position: absolute; top:0; left: 0; " width="100%" height="100%" src="https://www.facebook.com/plugins/video.php?href={{$block['url']}}/&show_text=0&width=476" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"
+    frameborder="0" allowfullscreen></iframe>
+</div>
+    {{-- @endif --}}
+
+    
