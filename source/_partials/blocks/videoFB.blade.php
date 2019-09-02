5841030a22aa792cd2f886bec06896d56dd6e673
WHS-Techs <ohmy@whs.ac.nz>
2019-09-02 16:13:21 +1200

    fix fb video link

--- source/_partials/blocks/videoFB.blade.php
+++ source/_partials/blocks/videoFB.blade.php
@@ -10,7 +10,7 @@
     {{-- @if($block["url"]) --}}
     
 <div style="height: 0; position: relative; padding-bottom: 56.25%">
-        <iframe style="position: absolute; top:0; left: 0; " width="100%" height="100%" src="https://www.facebook.com/plugins/video.php?href={{$block['url']}}/&show_text=0&width=476" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"
+        <iframe style="position: absolute; top:0; left: 0; " width="100%" height="100%" src="https://www.facebook.com/plugins/video.php?href={{$block['url']}}" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"
     frameborder="0" allowfullscreen></iframe>
 </div>
     {{-- @endif --}}
