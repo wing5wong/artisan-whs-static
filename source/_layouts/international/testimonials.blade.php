6596f2c0c1869ea1809880c907ddcf9e85cecbd8
WHS-Techs <ohmy@whs.ac.nz>
2020-06-23 09:38:15 +1200

    collapse international testimonials

--- source/_layouts/international/testimonials.blade.php
+++ source/_layouts/international/testimonials.blade.php
@@ -12,20 +12,28 @@
 
 @foreach($testimonials->where('international', true) as $student)
 <article class="py-3">
-        <h2 class="decorated py-3 mb-4">
-               {{ $student->title}}
-          <span class="text-muted">{{ $student->place ?? ""}}</span>
-            </h2>
-        <div class="row">
-          <div class="col-sm-12 col-md-4 col-lg-4">
-            {{-- <img src="{{ $student->image }}" width="140" height="186" alt=""> --}}
-            <img src="{{ $student->image }}" height="186" alt="">
-          </div>
-          <div class="col-sm-12 col-md-8 col-lg-8">
-            {!! $student !!}
-          </div>
-        </div>
-      </article>
+  
+  <div class="row">
+    <div class="col-sm-12 col-md-4 col-lg-4">
+      {{-- <img src="{{ $student->image }}" width="140" height="186" alt=""> --}}
+      <img src="{{ $student->image }}" height="186" alt="">
+    </div>
+    <div class="col-sm-12 col-md-8 col-lg-8">
+      <details>
+        <summary>
+          <h2 class="decorated d-table my-5">{{ $student->title}}
+            <br><small class="text-muted">{{ $student->place ?? ""}}</small>
+          </h2>
+        </summary>
+
+
+        {!! $student !!}
+
+      </details>
+
+    </div>
+  </div>
+</article>
 
 @endforeach
 
