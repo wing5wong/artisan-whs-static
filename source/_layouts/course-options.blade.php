4a5389f26593a1e7045ec76b2c5010fa02318cd0
WHS-Techs <ohmy@whs.ac.nz>
2020-08-10 15:19:10 +1200

    add quick reference to course options layout

--- source/_layouts/course-options.blade.php
+++ source/_layouts/course-options.blade.php
@@ -7,6 +7,9 @@
 
 {!! $page !!}
 
+
+
+
 @php
 $levels = [
 "9" => "Year 9",
@@ -17,19 +20,36 @@ $levels = [
 ];
 @endphp
 
+
+<div class="row">
+    <div class="col">
+        <h3>Quick Reference</h3>
+        <details>
+            <summary>Show Quick reference</summary>
+            <iframe
+                src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRd4r12Eku3C6QvYKC4tDF7O_qyldcjuPahlP7Xo6m76xtJlvB59MA_O1FVlLHl7mv2EYDI7kJwpfHo/pubhtml?gid=53379054&amp;single=true&amp;widget=true&amp;headers=false"
+                width="930" height="600"></iframe>
+        </details>
+    </div>
+</div>
+
+
+
 <ul class="nav nav-tabs" id="myTab" role="tablist">
 
     @foreach($levels as $level=>$levelTitle)
     <li class="nav-item">
-        <a class="nav-link @if($loop->first) active @endif" id="year-{{$level}}-tab" data-toggle="tab" href="#year-{{$level}}" role="tab"
-            aria-controls="year-{{$level}}" @if($loop->first)aria-selected="true"@endif>{{ $levelTitle }}</a>
+        <a class="nav-link @if($loop->first) active @endif" id="year-{{$level}}-tab" data-toggle="tab"
+            href="#year-{{$level}}" role="tab" aria-controls="year-{{$level}}"
+            @if($loop->first)aria-selected="true"@endif>{{ $levelTitle }}</a>
     </li>
     @endforeach
 </ul>
 <div class="tab-content" id="myTabContent">
 
     @foreach($levels as $level=>$levelTitle)
-    <div class="tab-pane fade @if($loop->first) show active @endif" id="year-{{$level}}" role="tabpanel" aria-labelledby="year-{{$level}}-tab">
+    <div class="tab-pane fade @if($loop->first) show active @endif" id="year-{{$level}}" role="tabpanel"
+        aria-labelledby="year-{{$level}}-tab">
 
         @php
         $mapped = $page->getTeachingFaculties($faculties)
@@ -64,7 +84,8 @@ $levels = [
                 {{ $faculty['faculty']->intro }}
                 @endif
                 <br>
-                Full details are available on the <a href="{{$faculty['faculty']->getPath()}}" class="btn btn-light mb-5">{{$faculty['faculty']->title}} faculty page</a>
+                Full details are available on the <a href="{{$faculty['faculty']->getPath()}}"
+                    class="btn btn-light mb-5">{{$faculty['faculty']->title}} faculty page</a>
 
 
 
