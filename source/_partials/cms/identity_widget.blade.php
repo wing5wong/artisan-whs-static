8a75128c2cfbc88fc31683b431dcba97cb92f7f6
WHS-Techs <ohmy@whs.ac.nz>
2020-08-12 13:51:41 +1200

    add identity redirect widget to admin index

--- source/_partials/cms/identity_widget.blade.php
+++ source/_partials/cms/identity_widget.blade.php
@@ -1,1 +1,14 @@
 <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" defer></script>
+
+<script defer>
+    if (window.netlifyIdentity) {
+        window.netlifyIdentity.on('init', (user) => {
+            if (!user) {
+                window.netlifyIdentity.on('login', () => {
+                    document.location.href = '/admin/';
+                });
+            }
+        });
+    }
+    </script>
+    
