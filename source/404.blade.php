ef18c7be896cc236c71da5fd125abec0c63842d9
WHS-Techs <ohmy@whs.ac.nz>
2020-08-26 09:38:12 +1200

    update 404 with links home and contact

--- source/404.blade.php
+++ source/404.blade.php
@@ -8,5 +8,7 @@ permalink: 404.html
 
 @section('content')
     <h1>404 Error - Schoolbag not found</h1>
+    <p>We couldn't find what you were looking for.</p>
+    <p>Please try again, or try the <a href="/">Home page</a> or <a href="/contact">contact us</a> for help.</p>
     <img src="https://res.cloudinary.com/whanganuihigh/image/upload/v1554157248/404.jpg" alt="" style="max-width:100%">
 @endsection
