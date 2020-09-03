2dd5a2ce1636eb33af4fe9b56bf31a359f565c73
WHS-Techs <ohmy@whs.ac.nz>
2020-09-03 16:12:43 +1200

    update vocational news config

--- source/_layouts/vocational-news.blade.php
+++ source/_layouts/vocational-news.blade.php
@@ -1,20 +1,27 @@
-@extends('_layouts.standard') 
+@extends('_layouts.standard')
 
-@section('title', $page->title) 
+@section('title', $page->title)
 
 @section('content')
 <main>
     <h1 class="decorated py-3 mb-4">{{ $page->title }}</h1>
 
     @if($page->closing_date)
-        {{ $page->closing_date }}
+    <p>
+        <strong> Closing Date:</strong> {{ date('F j, Y', $page->closing_date) }}
+    </p>
     @endif
-    @yield('postContent')
 
     @if($page->file)
     <a href="{{$page->file}}" download class="btn btn-light mb-5">Download PDF</a>
     @endif
 
-     </main>
-    @include('_partials.lastReviewed')
+    @if($page->closing_date or $page->file)
+    <hr>
+    @endif
+
+    @yield('postContent')
+
+</main>
+@include('_partials.lastReviewed')
 @endsection
