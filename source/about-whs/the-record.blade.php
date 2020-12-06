e9cb84a27e34f658a63441c5203c162be649c577
WHS-Techs <ohmy@whs.ac.nz>
2020-12-07 12:11:11 +1300

    paginate the record

---
+++ source/about-whs/the-record.blade.php
@@ -0,0 +1,46 @@
+---
+pagination:
+    collection: the_record
+    perPage: 25
+---
+@extends('_layouts.standard')
+@section('title', $page->title)
+@section('content')
+<h1 class="decorated">{{ $page->title }}</h1>
+
+@yield('postContent')
+
+
+@foreach($pagination->items as $record)
+
+{{ date('F j, Y', $record->date) }}
+{{$record->title}}
+<a href="{{$record->getPath()}}">Download</a>
+
+@endforeach
+
+@if ($previous = $pagination->previous)
+<a href="{{ $page->baseUrl }}{{ $pagination->first }}">&lt;&lt;</a>
+<a href="{{ $page->baseUrl }}{{ $previous }}">&lt;</a>
+@endif
+
+@foreach ($pagination->pages as $pageNumber => $path)
+@if($pagination->currentPage == $pageNumber)
+{{ $pageNumber }}
+</a>
+@else
+    @if( ($pagination->currentPage - $pageNumber <=5) or ($pageNumber - $pagination->currentPage <= 5))
+    <a href="{{ $page->baseUrl }}{{ $path }}">
+        {{ $pageNumber }}
+    </a>
+    @endif
+@endif
+@endforeach
+
+@if ($next = $pagination->next)
+<a href="{{ $page->baseUrl }}{{ $next }}">&gt;</a>
+<a href="{{ $page->baseUrl }}{{ $pagination->last }}">&gt;&gt;</a>
+@endif
+@endsection
+
+
