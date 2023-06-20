700499cb1a4838f6bf848e1999b093330852f04a
Sean Anderson <wing5wong@hotmail.com>
2023-06-20 12:57:40 +1200

    nullable image

--- source/curriculum.blade.php
+++ source/curriculum.blade.php
@@ -41,7 +41,7 @@
     @foreach($page->getTeachingFaculties($faculties) as $faculty)
 
     <div class="col-12 col-md-6 col-lg-4 d-flex align-items-center justify-content-center p-5 @if(!($faculty->image) and $r = rand(0, 2) == 2) bg-green @elseif(!($faculty->image) and rand(0, 2)==1) bg-yellow @endif "
-        style="background-image: url({{ str_replace("https://res.cloudinary.com/whanganuihigh/image/upload/","https://res.cloudinary.com/whanganuihigh/image/upload/c_scale,q_80,w_625/",$faculty->image)}}); background-size: cover; width: 100%; min-height: 350px;">
+        style="background-image: url({{ str_replace("https://res.cloudinary.com/whanganuihigh/image/upload/","https://res.cloudinary.com/whanganuihigh/image/upload/c_scale,q_80,w_625/",$faculty->image ?? '')}}); background-size: cover; width: 100%; min-height: 350px;">
         <h2 class="mb-0 h3 text-center">
             <a href="{{$faculty->getPath()}}" class="btn btn-light">
                 {{$faculty->title}} <br>
