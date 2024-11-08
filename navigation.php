42cd0fc12db12840106ba8b2788c92f85ec6938c
Sean Anderson <wing5wong@hotmail.com>
2024-11-08 16:23:27 +1300

    remove mobile phone rules

--- navigation.php
+++ navigation.php
@@ -140,10 +140,6 @@ return [
                 'url' => '/info-for-parents/byot'
             ],
             [
-                'title' => 'Mobile Phone Rules',
-                'url' => '/info-for-parents/mobile-phone-rules'
-            ],
-            [
                 'title' => 'Uniform',
                 'url' => '/info-for-parents/uniform'
             ],
