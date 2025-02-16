ed91b94f74283a9e9e7486277b63a5073135b9f7
Sean Anderson <wing5wong@hotmail.com>
2025-02-17 09:53:38 +1300

    add other fonts

--- source/extracurricular-activities/school-productions/chicago.blade.php
+++ source/extracurricular-activities/school-productions/chicago.blade.php
@@ -12,57 +12,87 @@
             font-weight: bold;
             src: url("/assets/production_chicago/MinionPro-Regular.otf") format("opentype");
         }
+
+        @font-face {
+            font-family: LTCBodoni;
+            src: url("/assets/production_chicago/LTC Bodoni 175 W01 Regular.ttf") format("truetype");
+        }
+
+        @font-face {
+            font-family: AlkalineBold;
+            font-weight: bold;
+            src: url("/assets/production_chicago/AlkalineBold.otf") format("opentype");
+        }
+
+        .chicago {
+            font-family: LTCBodoni
+        }
+
+        .header {
+            border-top: 2px solid #111;
+            border-bottom: 2px solid #111;
+            font-family: AlkalineBold
+        }
+
+        .header-gold {
+            color: #ca974d
+        }
+
+        .header-purple {
+            color: #733b96
+        }
     </style>
-    <h1 class="decorated py-3 mb-4">Chicago</h1>
+    <div class="chicago">
+        <img src="/assets/production_chicago/header with credits_black.png" alt="">
 
-    WHS Production Contract
-    Important Dates
-    AUDITIONS - October 18TH 2024 2PM -5PM
-    CAST LIST POSTED - OCTOBER 25TH
+        <img src="/assets/production_chicago/chicago header_gold.png" alt="">
 
-    <div class="purple-circle">
-        PLEASE JOIN
-        CHICAGO - WHS PRODUCTION '25
-        FACEBOOK PAGE FOR
-        The Latest Rehearsal Information
+        WHS Production Contract
+        Important Dates
+        AUDITIONS - October 18TH 2024 2PM -5PM
+        CAST LIST POSTED - OCTOBER 25TH
+
+        <div class="purple-circle">
+            PLEASE JOIN
+            CHICAGO - WHS PRODUCTION '25
+            FACEBOOK PAGE FOR
+            The Latest Rehearsal Information
+        </div>
+        BY ARRANGEMENT WITH ORiGiN™ THEATRICAL
+        ON BEHALF OF SAMUEL FRENCH, A Concord Theatricals Company
+
+        <h2 class="header header-gold">Performances</h2>
+        WEDNESDAY, 7th of May
+        THURSDAY, 8 of May
+        FRIDAY, 9th of May
+        Venue: ROYAL WHANGANUI OPERA HOUSE
+
+        <h2 class="header header-gold">Summary</h2>
+        In "Roaring Twenties" Chicago, chorine Roxie Hart murders
+        a faithless lover and convinces her hapless husband, Amos, to
+        take the rap ... until he finds out he's been duped and turns on
+        Roxie. Convicted and sent to death row, Roxie and another
+        "Merry Murderess," Velma Kelly, vie for the spotlight and
+        the headlines, ultimately joining forces in search of the
+        "American Dream": fame, fortune, and acquittal.
+        CONTENT WARNINGS: PG11 +
+        Adult Themes, Drinking, Drugs & Smoking, Mild Language, Depictions
+        of Violence/Death, Sexual Content
+
+
+        <h2 class="header header-gold">Cast List</h2>
+
+        <h2 class="header header-gold">Character Descriptions</h2>
+
+        <h2 class="header header-gold">History</h2>
+        Chicago first opened on Broadway at the 46th Street Theatre
+        in New York City on June 3, 1975. The production, directed
+        and choreographed by Bob Fosse, ran for 936 performances.
+        On Nov 14, 1996, a revival of the show opened on Broadway
+        at the Richard Rodgers Theatre. It later transferred to
+        Shubert Theatre, and then to the Ambassador Theatre,
+        where, more than two decades later, it continues to run,
+        logging nearly 10,000 performances
+
     </div>
-    BY ARRANGEMENT WITH ORiGiN™ THEATRICAL
-    ON BEHALF OF SAMUEL FRENCH, A Concord Theatricals Company
-    <img src="/assets/production_chicago/header with credits_black.png" alt="">
-    <img src="/assets/production_chicago/chicago header_gold.png" alt="">
-
-    <h2>Performances</h2>
-    WEDNESDAY, 7th of May
-    THURSDAY, 8 of May
-    FRIDAY, 9th of May
-    Venue: ROYAL WHANGANUI OPERA HOUSE
-
-    <h2>Summary</h2>
-    In "Roaring Twenties" Chicago, chorine Roxie Hart murders
-    a faithless lover and convinces her hapless husband, Amos, to
-    take the rap ... until he finds out he's been duped and turns on
-    Roxie. Convicted and sent to death row, Roxie and another
-    "Merry Murderess," Velma Kelly, vie for the spotlight and
-    the headlines, ultimately joining forces in search of the
-    "American Dream": fame, fortune, and acquittal.
-    CONTENT WARNINGS: PG11 +
-    Adult Themes, Drinking, Drugs & Smoking, Mild Language, Depictions
-    of Violence/Death, Sexual Content
-
-
-    <h2>Cast List</h2>
-
-    <h2>Character Descriptions</h2>
-
-    <h2>History</h2>
-    Chicago first opened on Broadway at the 46th Street Theatre
-    in New York City on June 3, 1975. The production, directed
-    and choreographed by Bob Fosse, ran for 936 performances.
-    On Nov 14, 1996, a revival of the show opened on Broadway
-    at the Richard Rodgers Theatre. It later transferred to
-    Shubert Theatre, and then to the Ambassador Theatre,
-    where, more than two decades later, it continues to run,
-    logging nearly 10,000 performances
-
-
 @endsection
