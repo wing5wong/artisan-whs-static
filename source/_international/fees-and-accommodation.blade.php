382a5385662e57a2da774faa63ce8259404d6f87
Sean Anderson <wing5wong@hotmail.com>
2025-07-23 10:38:26 +1200

    layout

--- source/_international/fees-and-accommodation.blade.php
+++ source/_international/fees-and-accommodation.blade.php
@@ -2,8 +2,10 @@
 title: Fees and Accommodation
 date: 2024-11-25T10:59:00.000Z
 ---
-<p><em>The tuition fee covers four or part terms from the starting date for the student. Included in the school fee are the cost of class fees, textbooks, the international student photo, the yearbook and the orientation course at the start. All prices are in New Zealand dollars.</em></p>
+<p>The tuition fee covers four or part terms from the starting date for the student. Included in the school fee are the
+    cost of class fees, textbooks, the international student photo, the yearbook and the orientation course at the
+    start. All prices are in New Zealand dollars.</p>
 
-<a href="https://whanganuihigh.enrol.school.nz" class="btn btn-light">Apply online</a>
-<br />
-<a href="https://res.cloudinary.com/whanganuihigh/image/upload/v1753150766/International/Whanganui_High_School_Fees_2026.pdf" class="btn btn-light">2026 Fee Schedule</a>
+<a href="https://whanganuihigh.enrol.school.nz" class="btn btn-light mr-3">Apply online</a>
+<a href="https://res.cloudinary.com/whanganuihigh/image/upload/v1753150766/International/Whanganui_High_School_Fees_2026.pdf"
+    class="btn btn-light">2026 Fee Schedule</a>
