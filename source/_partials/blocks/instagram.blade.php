e5ab6bb38efd2d3c4f226c405bd12d188277e480
Sean Anderson <wing5wong@hotmail.com>
2025-07-23 09:36:57 +1200

    update social media blocks

--- source/_partials/blocks/instagram.blade.php
+++ source/_partials/blocks/instagram.blade.php
@@ -3,9 +3,9 @@
         <h2>{{ $block['heading'] }}</h2>
     @endif
     <a href="{{ $block['url'] }}" class="btn btn-lg btn-fb" style="display: flex; gap: 1em; align-items: center;">
-        <i class="fab fa-facebook-f"
+        <i class="fab fa-instagram"
             style="
-                    color: white;
+    color: white;
     background-color: #3b5998;
     font-size: 20px;
     width: 40px;
@@ -14,7 +14,9 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    color: white;
+    background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%);
             "></i>
-        Facebook
+        Instagram
     </a>
 </section>
