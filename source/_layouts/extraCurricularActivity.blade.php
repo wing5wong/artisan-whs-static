d951ae5961165b0179dfe818a2c91d72a33b889b
Sean Anderson <wing5wong@hotmail.com>
2025-12-05 11:22:13 +1300

    gallery to extra curricular activity layout

--- source/_layouts/extraCurricularActivity.blade.php
+++ source/_layouts/extraCurricularActivity.blade.php
@@ -1,154 +1,183 @@
 @extends('_layouts.standard')
 @section('title', $page->title)
 @section('content')
-<h1 class="decorated py-3 mb-4">{{ $page->title }}</h1>
+    <h1 class="decorated py-3 mb-4">{{ $page->title }}</h1>
 
 
-@php
-$ecArea = $extracurricular_areas->firstWhere('title', $page->extracurricular_area);
-@endphp
+    @php
+        $ecArea = $extracurricular_areas->firstWhere('title', $page->extracurricular_area);
+    @endphp
 
-<ul class="nav">
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item">
-                <a href="/curriculum/extracurricular/">Extracurricular</a>
-            </li>
-            <li class="breadcrumb-item">
-                <a href="{{$ecArea->getPath()}}">{{$ecArea->title}}</a>
-            </li>
-        </ol>
-    </nav>
-</ul>
-
-
-@include('_partials.page.feature-image')
+    <ul class="nav">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="/curriculum/extracurricular/">Extracurricular</a>
+                </li>
+                <li class="breadcrumb-item">
+                    <a href="{{ $ecArea->getPath() }}">{{ $ecArea->title }}</a>
+                </li>
+            </ol>
+        </nav>
+    </ul>
 
 
-@php
-$personInCharge = $staff->firstWhere('title', $page->person_in_charge);
-@endphp
-@if(!($page->people) and $personInCharge)
-<h3>Person in charge</h3>
-<table>
-    <thead>
-        <tr>
-            <th>Name</th>
-            @if($personInCharge->phone)
-            <th>Phone</th>
-            @endif
-            @if($personInCharge->email)
-            <th>Email</th>
-            @endif
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>{{ $personInCharge->title ?? '' }}</td>
-            @if($personInCharge->phone)<td>{{ $personInCharge->phone ?? '' }}</td>@endif
-            @if($personInCharge->email)<td>{{ $personInCharge->email ?? '' }}</td>@endif
-        </tr>
-    </tbody>
-</table>
-@endif
+    @include('_partials.page.feature-image')
 
 
-<h3>Key Information</h3>
+    @php
+        $personInCharge = $staff->firstWhere('title', $page->person_in_charge);
+    @endphp
+    @if (!$page->people and $personInCharge)
+        <h3>Person in charge</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    @if ($personInCharge->phone)
+                        <th>Phone</th>
+                    @endif
+                    @if ($personInCharge->email)
+                        <th>Email</th>
+                    @endif
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{ $personInCharge->title ?? '' }}</td>
+                    @if ($personInCharge->phone)
+                        <td>{{ $personInCharge->phone ?? '' }}</td>
+                    @endif
+                    @if ($personInCharge->email)
+                        <td>{{ $personInCharge->email ?? '' }}</td>
+                    @endif
+                </tr>
+            </tbody>
+        </table>
+    @endif
 
-<table>
-    <thead>
-    </thead>
-    <tbody>
-        @if($page->people)
-        @php
-        $involvedStaff = collect($page->people)->map(function($person) use ($staff){
-        return ['person' => $staff->firstWhere('title', $person["name"]), 'role' => $person["role"]];
-        })->filter(function($st){
-        return !is_null($st['person']);
-        });
-        @endphp
-        <tr>
-            <th>Name</th>
-            <th>Role</th>
-            <th>Phone</th>
-            <th>Email</th>
-        </tr>
-        @foreach($involvedStaff as $involved)
-        <tr>
-            <td>{{ $involved["person"]["title"] }}</td>
-            <td>{{ $involved["role"] }}</td>
-            <td>{{ isset($involved["person"]["phone"]) ? $involved["person"]->phone : '' }}</td>
-            <td>{{ isset($involved["person"]["email"]) ? $involved["person"]->email : '' }}</td>
-        </tr>
-        @endforeach
-        @endif
 
-        @if($page->cost)
-        <tr>
-            <th>Cost</th>
-            <td colspan="3"> {{ $page->cost}}</td>
-        </tr>
-        @endif
+    <h3>Key Information</h3>
 
-        @if($page->terms)
-        <tr>
-            <th>
-                When
-            </th>
-            <td colspan="3">
-                @foreach($page->terms as $term)
-                {{ $term }}@if(!$loop->last), @endif
+    <table>
+        <thead>
+        </thead>
+        <tbody>
+            @if ($page->people)
+                @php
+                    $involvedStaff = collect($page->people)
+                        ->map(function ($person) use ($staff) {
+                            return [
+                                'person' => $staff->firstWhere('title', $person['name']),
+                                'role' => $person['role'],
+                            ];
+                        })
+                        ->filter(function ($st) {
+                            return !is_null($st['person']);
+                        });
+                @endphp
+                <tr>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                </tr>
+                @foreach ($involvedStaff as $involved)
+                    <tr>
+                        <td>{{ $involved['person']['title'] }}</td>
+                        <td>{{ $involved['role'] }}</td>
+                        <td>{{ isset($involved['person']['phone']) ? $involved['person']->phone : '' }}</td>
+                        <td>{{ isset($involved['person']['email']) ? $involved['person']->email : '' }}</td>
+                    </tr>
                 @endforeach
-            </td>
-        </tr>
-        @endif
+            @endif
 
-        @if($page->uniform)
-        <tr>
-            <th>Uniform/Equipment</th>
-            <td colspan="3">{{ $page->uniform}}</td>
-        </tr>
-        @endif
+            @if ($page->cost)
+                <tr>
+                    <th>Cost</th>
+                    <td colspan="3"> {{ $page->cost }}</td>
+                </tr>
+            @endif
 
+            @if ($page->terms)
+                <tr>
+                    <th>
+                        When
+                    </th>
+                    <td colspan="3">
+                        @foreach ($page->terms as $term)
+                            {{ $term }}@if (!$loop->last)
+                                ,
+                            @endif
+                        @endforeach
+                    </td>
+                </tr>
+            @endif
 
-    </tbody>
-</table>
-
-
-@yield('postContent')
-
-
-@php
-$recentNews = $news->filter(function($article) use ($page){
-return in_array($page->title, $article->extracurricular_activities ?? []);
-})
-->take(6);
-@endphp
-@if(count($recentNews))
-
-<h3>Recently in the news</h3>
-<div class="row">
-    @foreach($recentNews as $n)
-    <div class="col-sm-12 col-md-6 col-lg-4">
-        <a href="{{$n->getPath()}}">
-            <img src="{{str_replace("https://res.cloudinary.com/whanganuihigh/image/upload/","https://res.cloudinary.com/whanganuihigh/image/upload/c_fill,g_face,q_80,w_250,h_170/", $page->featureImageSrc($n))}}" srcset="
-{{str_replace("https://res.cloudinary.com/whanganuihigh/image/upload/","https://res.cloudinary.com/whanganuihigh/image/upload/c_fill,g_face,q_80,w_400,h_360/", $page->featureImageSrc($n))}} 400w,
-{{str_replace("https://res.cloudinary.com/whanganuihigh/image/upload/","https://res.cloudinary.com/whanganuihigh/image/upload/c_fill,g_face,q_80,w_250,h_170/", $page->featureImageSrc($n))}} 250w
-" sizes="(min-width: 800px) 400px, 250px" width="600" alt="{{$page->featureImageAlt($n) ?? ''}}" title="{{ $page->featureImageDescription($n)}}" style="max-width: 100%">
-            {{$n->title}}
-        </a>
-    </div>
-    @endforeach
-</div>
-@endif
+            @if ($page->uniform)
+                <tr>
+                    <th>Uniform/Equipment</th>
+                    <td colspan="3">{{ $page->uniform }}</td>
+                </tr>
+            @endif
 
 
-@if($page->blocks)
-@foreach($page->blocks as $block)
-@include('_partials.blocks.' . $block["type"], ['block'=>$block])
-@endforeach
-@endif
+        </tbody>
+    </table>
 
-@include('_partials.lastReviewed')
+
+    @yield('postContent')
+
+    @if (is_array($page->image_gallery))
+        <div class="image-gallery row">
+            @foreach ($page->image_gallery as $image)
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <img src="{{ str_replace('https://res.cloudinary.com/whanganuihigh/image/upload/', 'https://res.cloudinary.com/whanganuihigh/image/upload/c_scale,q_80,w_300/', $image['image']) }}"
+                        @isset($image['description'])alt="{{ $image['description'] }}"@endisset
+                        @isset($image['description'])title="{{ $image['description'] }}"@endisset
+                        data-image-url="{{ $image['image'] }}">
+                </div>
+            @endforeach
+        </div>
+    @endif
+
+
+    @php
+        $recentNews = $news
+            ->filter(function ($article) use ($page) {
+                return in_array($page->title, $article->extracurricular_activities ?? []);
+            })
+            ->take(6);
+    @endphp
+    @if (count($recentNews))
+
+        <h3>Recently in the news</h3>
+        <div class="row">
+            @foreach ($recentNews as $n)
+                <div class="col-sm-12 col-md-6 col-lg-4">
+                    <a href="{{ $n->getPath() }}">
+                        <img src="{{ str_replace('https://res.cloudinary.com/whanganuihigh/image/upload/', 'https://res.cloudinary.com/whanganuihigh/image/upload/c_fill,g_face,q_80,w_250,h_170/', $page->featureImageSrc($n)) }}"
+                            srcset="
+{{ str_replace('https://res.cloudinary.com/whanganuihigh/image/upload/', 'https://res.cloudinary.com/whanganuihigh/image/upload/c_fill,g_face,q_80,w_400,h_360/', $page->featureImageSrc($n)) }} 400w,
+{{ str_replace('https://res.cloudinary.com/whanganuihigh/image/upload/', 'https://res.cloudinary.com/whanganuihigh/image/upload/c_fill,g_face,q_80,w_250,h_170/', $page->featureImageSrc($n)) }} 250w
+"
+                            sizes="(min-width: 800px) 400px, 250px" width="600"
+                            alt="{{ $page->featureImageAlt($n) ?? '' }}" title="{{ $page->featureImageDescription($n) }}"
+                            style="max-width: 100%">
+                        {{ $n->title }}
+                    </a>
+                </div>
+            @endforeach
+        </div>
+    @endif
+
+
+    @if ($page->blocks)
+        @foreach ($page->blocks as $block)
+            @include('_partials.blocks.' . $block['type'], ['block' => $block])
+        @endforeach
+    @endif
+
+    @include('_partials.lastReviewed')
 
 @endsection
